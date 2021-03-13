5230cacbdd1d9623f4acd0abbc3be059fd7056a8
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-13 17:27:09 +0100

    added a lower then one throw error and catch error failsafe

--- chapters/HelloWorld/src/be/jochenhansoul/methodengebruiken/IsPrimeMethod.java
+++ chapters/HelloWorld/src/be/jochenhansoul/methodengebruiken/IsPrimeMethod.java
@@ -2,15 +2,19 @@ package be.jochenhansoul.methodengebruiken;
 
 public class IsPrimeMethod {
     public static void main(String[] args) {
-        for (int i = 1; i < 20; i++) {
-            System.out.println(i + ": " + isPrime(i));
+        for (int i = -2; i < 8; i++) {
+            try {
+                System.out.println(i + ": " + isPrime(i));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
     public static boolean isPrime(int n) {
         if (n == 1) {
             return false;
-        } {
+        } else if (n > 1){
             int i = 2;
             boolean dividable = true;
             while (i < (n / 2 + 1) && dividable) {
@@ -20,6 +24,8 @@ public class IsPrimeMethod {
                 i++;
             }
             return dividable;
+        } else {
+            throw new IllegalArgumentException("Number may not be lower then one");
         }
     }
 }
