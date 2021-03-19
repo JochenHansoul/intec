70606ccf4e449daaa72425d7954724a18017f3e1
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-19 14:03:02 +0100

    added the Euclidean algorithm for finding the greatest common divider

--- tests/t01_first_test/src/be/jochenhansoul/Second.java
+++ tests/t01_first_test/src/be/jochenhansoul/Second.java
@@ -18,4 +18,24 @@ public class Second {
         }
         return lowest;
     }
+
+    // euclidean algorithm
+    /*
+    Noem het grootste van de beide getallen A {\displaystyle A} A, het andere B {\displaystyle B} B.
+    Trek B {\displaystyle B} B net zo vaak van A {\displaystyle A} A af totdat er 0 overblijft of een getal kleiner dan B {\displaystyle B} B ( A mod B {\displaystyle A\!\!\!\mod B} {\displaystyle A\!\!\!\mod B}).
+    Als er 0 overblijft, is B {\displaystyle B} B de ggd.
+    Zo niet, herhaal dan het algoritme met B {\displaystyle B} B en wat er van A {\displaystyle A} A over is.
+     */
+    public static int euclideanAlgorithm(int first, int second) {
+        int lowest = Math.min(first, second);
+        int highest = Math.max(first, second);
+        while (highest >= lowest) {
+            highest -= lowest;
+        }
+        if (highest == 0) {
+            return lowest;
+        } else {
+            return euclideanAlgorithm(lowest, highest);
+        }
+    }
 }
