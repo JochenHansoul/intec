da5677d9b22b3b46fd2f04b77d0ff181075b9418
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-29 14:22:19 +0200

    just added a simple boolean array

---
+++ chapters/c07_arrays/src/be/jochenhansoul/opdracht1/Opdracht4.java
@@ -0,0 +1,10 @@
+package be.jochenhansoul.opdracht1;
+
+public class Opdracht4 {
+    public static void main(String[] args) {
+        boolean[] booleans = {true, false, true, false, true, false};
+        for (boolean b : booleans) {
+            System.out.println(b);
+        }
+    }
+}
