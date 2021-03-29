6db0aa0c696c58b1f51e2e7bd6ca7c519ce489f6
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-29 14:15:19 +0200

    created the excercise and method the get the value of a word in scrabble

--- chapters/c07_arrays/src/be/jochenhansoul/opdracht4/Scrabble.java
+++ chapters/c07_arrays/src/be/jochenhansoul/opdracht4/Scrabble.java
@@ -1,2 +1,67 @@
-package be.jochenhansoul.opdracht4;public class Scrabble {
+package be.jochenhansoul.opdracht4;
+
+import java.util.Scanner;
+
+public class Scrabble {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean continueGame = true;
+        while (continueGame) {
+            System.out.println("enter a word:");
+            String word = input.nextLine();
+            System.out.println(valueWord(word));
+            System.out.println("continue? true/false");
+            continueGame = input.nextBoolean();
+            input.nextLine();
+        }
+        input.close();
+    }
+
+    public static int valueWord(String word) {
+        int value = 0;
+        for (char c : word.toUpperCase().toCharArray()) {
+            value += valueLetter(c);
+        }
+        return value;
+    }
+
+    public static int valueLetter(char c) {
+        switch (c) {
+            case 'E':
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'N':
+            case 'R':
+            case 'T':
+            case 'L':
+            case 'S':
+            case 'U':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
+    }
 }
