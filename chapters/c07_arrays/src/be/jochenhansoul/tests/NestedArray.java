da18998e33d012e6206d15343515d04da6666cc8
JochenHansoul <jochenhansoul101@hotmail.com>
2021-03-29 14:17:35 +0200

    just a test of printing an array will print the reference or the content

--- chapters/c07_arrays/src/be/jochenhansoul/tests/NestedArray.java
+++ chapters/c07_arrays/src/be/jochenhansoul/tests/NestedArray.java
@@ -1,4 +1,11 @@
 package be.jochenhansoul.tests;
 
+import java.util.Arrays;
+
 public class NestedArray {
+    public static void main(String[] args) {
+        int[] ar1 = new int[] {1, 2, 3, 4};
+        System.out.println(ar1); // reference to array
+        System.out.println(Arrays.toString(ar1));
+    }
 }
