5b4f723e70a19a70c40edef6ecabeebf14115b24
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-09 19:21:07 +0200

    make all fields private, changed Array Person[] to List<Person>, added setters and methods to add and remove proxies

--- exercises/e01_bank/src/be/jochenhansoul/bank/BankAccount.java
+++ exercises/e01_bank/src/be/jochenhansoul/bank/BankAccount.java
@@ -1,30 +1,40 @@
 package be.jochenhansoul.bank;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.ArrayList;
 
 public class BankAccount {
-    String id;
-    Person owner;
-    String currency;
-    Person[] proxies;
-    int lowerLimit = 0;
-    BigDecimal balance = BigDecimal.valueOf(0);
-    String[] history = new String[0];
+    private String id;
+    private Person owner;
+    private String currency;
+    private List<Person> proxies;
+    private int lowerLimit = 0;
+    private BigDecimal balance = BigDecimal.valueOf(0);
+    private List<Transaction> history = new ArrayList();
 
     public BankAccount(String id, Person owner, String currency) {
-        this(id, owner, currency, new Person[0]);
+        this(id, owner, currency, new ArrayList());
     }
 
     public BankAccount(
         String id,
         Person owner,
         String currency,
-        Person[] proxies) {
+        ArrayList proxies) {
 
         this.id = id;
         this.owner = owner;
         this.currency= currency;
         this.proxies = proxies;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public void setLowerLimit(int lowerLimit) {
+        this.lowerLimit = lowerLimit;
     }
 
     public String getId() {
@@ -40,19 +50,43 @@ public class BankAccount {
     }
 
     public Person[] getProxies() {
-        return this.proxies;
+        return this.proxies.toArray(new Person[0]).clone();
     }
 
     public int getLowerLimit() {
         return this.lowerLimit;
     }
     
-    public BigDecimal getBalance() {
-        return this.balance;
+    public String getBalance() {
+        return this.balance.toString();
+    }
+
+    public void deposit(Person person, String amount) {
+        // todo
+        this.balance.add(new BigDecimal(amount));
+    }
+
+    public void widraw(String amount) {
+        // todo
+        BigDecimal toBeSubtracted = new BigDecimal(amount);
+        if (this.balance.subtract(toBeSubtracted).compareTo(new BigDecimal(this.lowerLimit)) >= 0) {
+            this.balance.subtract(toBeSubtracted);
+        } else {
+            // todo
+        }
     }
 
     public String[] getHistory() {
-        return this.history;
+        //return this.history.toArray(new String[0]).clone();
+        return new String[0]; // todo
+    }
+
+    public void addProxie(Person person) {
+        this.proxies.add(person);
+    }
+
+    public void removeProxie(int i) {
+        this.proxies.remove(i);
     }
 
     @Override
