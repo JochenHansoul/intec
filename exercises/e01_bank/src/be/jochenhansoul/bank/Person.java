5aa998e6ae0dbf8564474b922675df97f9809e4b
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-09 17:59:35 +0200

    changed the ContactInfo[] array to a list, fixed errors and added setters

--- exercises/e01_bank/src/be/jochenhansoul/bank/Person.java
+++ exercises/e01_bank/src/be/jochenhansoul/bank/Person.java
@@ -1,21 +1,45 @@
 package be.jochenhansoul.bank;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Person {
+    private static int idCounter = 0;
+
+    public final int ID;
     private String firstName;
     private String lastName;
     private Address address;
-    private ContactInfo[] contactNumbers;
+    private List<ContactInfo> contactInfos;
+
+    public Person(String firstName, String lastName, Address address) {
+        this(firstName, lastName, address, new ArrayList());
+    }
 
     public Person(
-        String firstname,
+        String firstName,
         String lastName,
         Address address,
-        ContactInfo[] contactNumbers) {
+        ArrayList<ContactInfo> contactInfos) {
 
+        this.ID = ++idCounter;
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
-        this.contactNumbers = contactNumbers;
+        this.contactInfos = contactInfos;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public String getFirstName() {
@@ -30,12 +54,20 @@ public class Person {
         return this.address;
     }
 
-    public ContactInfo[] getPhoneNumbers() {
-        return this.contactNumbers;
+    public ContactInfo[] getContacts(int i) {
+        return this.contactInfos.toArray(new ContactInfo[0]).clone();
+    }
+
+    public void addContact(ContactInfo contactInfo) {
+        this.contactInfos.add(contactInfo);
+    }
+
+    public void removeContact(int i) {
+        this.contactInfos.remove(i);
     }
 
     @Override
     public String toString() {
-        return this.firstName + " " + this.lastName;
+        return this.firstName + " " + this.lastName + " " + this.address;
     }
 }
