6eb7a2bc50520d255f3a068c3762f6fc0bbe0b1e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-13 19:37:50 +0200

    made app

--- chapters/c10_overerving/src/be/jochenhansoul/opdracht1/App.java
+++ chapters/c10_overerving/src/be/jochenhansoul/opdracht1/App.java
@@ -1,2 +1,14 @@
-package be.jochenhansoul.opdracht1;public class App {
+package be.jochenhansoul.opdracht1;
+
+import be.jochenhansoul.opdracht1.data.Rectangle;
+import be.jochenhansoul.opdracht1.data.Square;
+
+public class App {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 5, 1, 1);
+        Square square = new Square(4, 10, 10);
+        rectangle.setHeight(7);
+        square.setHeight(5);
+        System.out.println(square.getWidth());
+    }
 }
