7b82f49c6f9790cdebfd1fe55f2157425ae49a07
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-13 19:37:18 +0200

    made squaire class

--- chapters/c10_overerving/src/be/jochenhansoul/opdracht1/data/Square.java
+++ chapters/c10_overerving/src/be/jochenhansoul/opdracht1/data/Square.java
@@ -1,8 +1,24 @@
-package be.jochenhansoul.opdracht1.date;
+package be.jochenhansoul.opdracht1.data;
 
 public class Square extends Rectangle {
 
     public Square(int size, int x, int y) {
         super(size, size, x, y);
     }
+
+    @Override
+    public void setHeight(int height) {
+        setSize(height);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        setSize(width);
+    }
+
+    public void setSize(int size) {
+        super.setHeight(size);
+        super.setWidth(size);
+    }
+
 }
