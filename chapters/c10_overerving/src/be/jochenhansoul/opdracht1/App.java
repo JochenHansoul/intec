3b59f709a72a53e84b31c3b12df259ae854b2bbf
JochenHansoul <jochenhansoul101@hotmail.com>
2021-04-15 15:03:10 +0200

    change order of rectangle and squire

--- chapters/c10_overerving/src/be/jochenhansoul/opdracht1/App.java
+++ chapters/c10_overerving/src/be/jochenhansoul/opdracht1/App.java
@@ -6,9 +6,11 @@ import be.jochenhansoul.opdracht1.data.Square;
 public class App {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(10, 5, 1, 1);
-        Square square = new Square(4, 10, 10);
         rectangle.setHeight(7);
+
+        Square square = new Square(4, 10, 10);
         square.setHeight(5);
+
         System.out.println(square.getWidth());
     }
 }
