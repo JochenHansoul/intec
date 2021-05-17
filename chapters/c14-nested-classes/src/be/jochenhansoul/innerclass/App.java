3b05daecc23c88bbbdc40f8f8fa7408ecead1c26
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-17 13:07:35 +0200

    added main method and content to test the Musician class and nested Instrument class

--- chapters/c14-nested-classes/src/be/jochenhansoul/innerclass/App.java
+++ chapters/c14-nested-classes/src/be/jochenhansoul/innerclass/App.java
@@ -1,4 +1,12 @@
 package be.jochenhansoul.innerclass;
 
 public class App {
+    public static void main(String[] args) {
+        Musician musician = new Musician();
+        musician.play();
+
+        // instnatiate inner class
+        Musician.Instrument instrument = musician.new Instrument();
+        instrument.makeSound();
+    }
 }
