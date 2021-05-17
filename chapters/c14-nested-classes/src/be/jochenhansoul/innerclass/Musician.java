76c950ce818b83305a11824995525f6a76ebc0ea
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-17 12:02:10 +0200

    added inner class Instrument with method makeSound() and added method play() to Musician

--- chapters/c14-nested-classes/src/be/jochenhansoul/innerclass/Musician.java
+++ chapters/c14-nested-classes/src/be/jochenhansoul/innerclass/Musician.java
@@ -1,4 +1,14 @@
 package be.jochenhansoul.innerclass;
 
 public class Musician {
+    public class Instrument {
+        public void makeSound() {
+            System.out.println("instrument makes sound");
+        }
+    }
+
+    public void play() {
+        Instrument instrument = new Instrument();
+        instrument.makeSound();
+    }
 }
