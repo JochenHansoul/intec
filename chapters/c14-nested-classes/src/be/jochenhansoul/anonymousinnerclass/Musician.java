3ff352d39e31637366c8f94d7a2aa319a3d2b086
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-17 13:58:26 +0200

    added content of play() method in anonymousinnerclass/Musician class so that an anonymouse object is made and it's method is used

--- chapters/c14-nested-classes/src/be/jochenhansoul/anonymousinnerclass/Musician.java
+++ chapters/c14-nested-classes/src/be/jochenhansoul/anonymousinnerclass/Musician.java
@@ -3,5 +3,11 @@ package be.jochenhansoul.localinnerclass;
 public class Musician {
 
     public void play() {
+        Playable instrument = new Playable() {
+            public void makeSound() {
+                System.out.println("instrument makes sound");
+            }
+        }
+        instrument.makeSound();
     }
 }
