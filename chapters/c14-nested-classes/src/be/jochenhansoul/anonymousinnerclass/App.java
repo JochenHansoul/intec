141d0f025c8d1b02772fe4bdb83455cb9aef877e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-17 14:04:14 +0200

    created App class in anonymousinnerclass and added main method and content

---
+++ chapters/c14-nested-classes/src/be/jochenhansoul/anonymousinnerclass/App.java
@@ -0,0 +1,8 @@
+package be.jochenhansoul.anonymousinnerclass;
+
+public class App {
+    public static void main(String[] args) {
+        Musician musician = new Musician();
+        musician.play();
+    }
+}
