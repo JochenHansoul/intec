02fc46453f4c69a6a01eb3bce993b597885ca916
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-20 11:32:33 +0200

    created Duo class in module c17-generics and added code

---
+++ chapters/c17-generics/src/be/jochenhansoul/generics/Duo.java
@@ -0,0 +1,33 @@
+package be.jochenhansoul.generics;
+
+public class Duo<T> {
+    private T first;
+    private T second;
+
+    public Duo(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void swap() {
+        T temp = this.first;
+        this.first = this.second;
+        this.second = temp;
+    }
+}
