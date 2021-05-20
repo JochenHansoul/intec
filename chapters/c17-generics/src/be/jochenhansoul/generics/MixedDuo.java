4d2cb0306f5aff754f7f7ea037a79d5967e88976
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-20 11:59:14 +0200

    created MixedDuo class in c17-generics module and added code

---
+++ chapters/c17-generics/src/be/jochenhansoul/generics/MixedDuo.java
@@ -0,0 +1,27 @@
+package be.jochenhansoul.generics;
+
+public class MixedDuo<T, U> {
+    private T first;
+    private U second;
+
+    public Duo(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return this.first;
+    }
+
+    public U getSecond() {
+        return this.second;
+    }
+}
