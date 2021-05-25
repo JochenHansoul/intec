17e18e399f8200e9f275e4113ae2b8fdd15cbd7e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-25 11:52:57 +0200

    added main in TextApp. Main creates a TextPrinter object and then uses the method printFilteredWord with a filter to print all words containing the letter 'e'

--- chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/TextApp.java
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/TextApp.java
@@ -1,4 +1,11 @@
 package be.jochenhansoul.opdracht1;
 
 public class TextApp {
+    public static void main(String[] args) {
+        TextPrinter tp = new TextPrinter(
+            "Hello this is an example of a sentence containing words");
+
+        System.out.println("***words containing 'e'***");
+        tp.printFilteredWords((s) -> s.contains("e"));
+    }
 }
