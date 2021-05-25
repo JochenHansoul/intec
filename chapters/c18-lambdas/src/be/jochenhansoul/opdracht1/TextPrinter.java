214ac728124768ceaf62113ccb03347e4d73c278
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-25 11:43:02 +0200

    added constructor to add a sentence and a method to print filtered words

--- chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/TextPrinter.java
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/TextPrinter.java
@@ -1,4 +1,17 @@
 package be.jochenhansoul.opdracht1;
 
 public class TextPrinter {
+    private String sentence;
+
+    public TextPrinter(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void printFilteredWords(WordFilter filter) {
+        for (String w : sentence.split(" ")) {
+            if (filter.isValid(w)) {
+                System.out.println(w);
+            }
+        }
+    }
 }
