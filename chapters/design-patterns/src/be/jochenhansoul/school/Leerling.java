18c1fd663a2e995453bd345115b8e827f78b767b
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 14:50:27 +0200

    added Leerling code and method to get info

--- chapters/design-patterns/src/be/jochenhansoul/school/Leerling.java
+++ chapters/design-patterns/src/be/jochenhansoul/school/Leerling.java
@@ -1,4 +1,23 @@
 package be.jochenhansoul.school;
 
+import java.lang.StringBuilder;
+
 public class Leerling {
+    private String name;
+    private String actie = "";
+    private String description = "";
+
+    public Leerling(String name) {
+        this.name = name;
+    }
+
+    public String getInfo(Modus modus) {
+        if (modus.equals(Modus.DAD)) {
+            return this.name;
+        } else if (modus.equals(Modus.ME)) {
+            return this.name + " " + this.actie;
+        } else {
+            return this.name + " " + this.actie + " " + this.description;
+        }
+    }
 }
