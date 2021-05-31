df1a5290363f77f5ff62bc8624829f370aa88f77
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 03:05:54 +0200

    added main to print the charakters in a string

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
@@ -1,4 +1,11 @@
 package be.jochenhansoul.opdracht1;
 
+import java.util.stream.*;
+
 public class Source {
+    public static void main(String[] args) {
+        String[] array1 = new String[]{"a", "b", "c", "d", "e"};
+        Stream.of(array1)
+            .forEach(System.out::println);
+    }
 }
