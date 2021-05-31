ffa02d978f538a0165804080e287bfff26d41290
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 03:37:34 +0200

    added cladd person with toString() method

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Person.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Person.java
@@ -1,4 +1,45 @@
 package be.jochenhansoul.opdracht1;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private int age;
+    private float weight;
+    private float length;
+
+    public Person (
+        String firstName,
+        String lastName,
+        Gender gender,
+        int age,
+        float weight,
+        float length) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+        this.length = length;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s, %s, age: %n, weight: %n, length: %n",
+            this.firstName,
+            this.lastName,
+            this.gender.toString(),
+            this.age,
+            this.weight,
+            this.length);
+    }
 }
