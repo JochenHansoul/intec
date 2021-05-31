76dc9e50a4980e23a16094fd1b07bea3db093cdc
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 21:53:36 +0200

    added main with a person array

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht3/App.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht3/App.java
@@ -1,4 +1,14 @@
 package be.jochenhansoul.opdracht3;
 
+import be.jochenhansoul.general.*;
+
 public class App {
+    public static void main(String[] args) {
+        Person[] persons = new Person[] {
+            new Person("first1", "last1", Gender.MALE, 1, 50F, 1.7F),
+            new Person("first2", "last2", Gender.OTHER, 2, 50F, 1.7F),
+            new Person("first3", "last3", Gender.FEMALE, 3, 50F, 1.7F),
+            new Person("first3", "last3", Gender.UNKNOWN, 3, 50F, 1.7F)
+        };
+    }
 }
