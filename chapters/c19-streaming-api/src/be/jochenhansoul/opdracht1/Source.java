d131aa54169ad8b26cdb45e57133155f5e81e2f6
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 03:17:28 +0200

    added part 2, code that prints all numbers from 0, 99 with a stream

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
@@ -4,8 +4,15 @@ import java.util.stream.*;
 
 public class Source {
     public static void main(String[] args) {
+        // part 1
         String[] array1 = new String[]{"a", "b", "c", "d", "e"};
         Stream.of(array1)
             .forEach(System.out::println);
+
+        // part 2
+        IntStream.range(0, 100)
+            .forEach((s) -> System.out.print(s + ", "));
+
+        // part 3
     }
 }
