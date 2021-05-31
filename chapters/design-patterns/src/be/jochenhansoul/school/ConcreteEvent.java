d17027439625d25c9f9b221573f1fb82b180c0c2
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 17:41:51 +0200

    added properties eventCategory and message and implemented the Event interface and the methods

--- chapters/design-patterns/src/be/jochenhansoul/school/ConcreteEvent.java
+++ chapters/design-patterns/src/be/jochenhansoul/school/ConcreteEvent.java
@@ -1,4 +1,26 @@
 package be.jochenhansoul.school;
 
-public class ConcreteEvent {
+public class ConcreteEvent implements Event {
+    private EventCategory category;
+    private Object message;
+
+    public ConcreteEvent(EventCategory category, Object message) {
+        this.category = category;
+        this.message = message;
+    }
+
+    @Override
+    public boolean isCategory(EventCategory category) {
+        return this.category == category;
+    }
+
+    @Override
+    public Object getInformation() {
+        return this.message;
+    }
+
+    @Override
+    public void setInformation(Object message) {
+        this.message = message;
+    }
 }
