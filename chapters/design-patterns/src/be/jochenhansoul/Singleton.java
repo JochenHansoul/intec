1c837bab7e13e15da84c90c126f8c7ff92ca49ef
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 11:09:52 +0200

    added the correct way of doing things as done by the gang of four

--- chapters/design-patterns/src/be/jochenhansoul/Singleton.java
+++ chapters/design-patterns/src/be/jochenhansoul/Singleton.java
@@ -8,15 +8,27 @@ wij >> geven de code het instance
 wij >> zet snoepjes(container) "constructor" waar de andere code er niet aankan
     ENCAPSULATION
 2. 'eASY' access to said instance
+
+this is invented by the group of four
+
+the new gang of four
+uncle bob (roger martin)
+linus thorvald
+
 */
 
 public class Singleton {
-    private static boolean alreadyMade = false;
+    private static Singleton instance = new Singleton();
+    //private static boolean alreadyMade = false;
 
     private Singleton() {
     }
 
-    public static getInstance() {
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    /*public static getInstance() {
         if (alreadyMade) {
             throw new Exception("only one instance of Singleton class allowed");
         } else {
@@ -24,4 +36,5 @@ public class Singleton {
             alreadyMade = true;
         }
     }
+    */
 }
