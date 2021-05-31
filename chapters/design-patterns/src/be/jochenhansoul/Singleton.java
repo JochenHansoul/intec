2f64d236ac7268941fec921ad6bb1143636d015e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 10:07:57 +0200

    added constructor and code to prevent the creation of many Singleton instances

--- chapters/design-patterns/src/be/jochenhansoul/Singleton.java
+++ chapters/design-patterns/src/be/jochenhansoul/Singleton.java
@@ -1,4 +1,12 @@
 package be.jochenhanoul;
 
 public class Singleton {
+    private static boolean alreadyMade = false;
+
+    public Singleton() {
+        if (alreadyMade) {
+            throw new Exception("only one instance of Singleton class allowed");
+        } else {
+            alreadyMade = true;
+        }
 }
