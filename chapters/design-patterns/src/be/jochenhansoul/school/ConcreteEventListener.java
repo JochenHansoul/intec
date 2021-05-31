2dac69cec8f33d3dd343d7dc33af835597cdf9bd
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 17:43:19 +0200

    implement'ed EventListener interface and added method onEvent

--- chapters/design-patterns/src/be/jochenhansoul/school/ConcreteEventListener.java
+++ chapters/design-patterns/src/be/jochenhansoul/school/ConcreteEventListener.java
@@ -1,4 +1,10 @@
 package be.jochenhansoul.school;
 
-public class ConcreteEventListener {
+public class ConcreteEventListener  implements EventListener {
+    @Override
+    public void onEvent(Event event) {
+        if (event.isCategory(EventCategory.DEFCON_FIVE)) {
+            System.out.println(event.getInformation());
+        }
+    }
 }
