ed8db1fb47a9828d22302f9b874d7dd9e398204b
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 17:45:20 +0200

    added main method and code to create an event, a bus object and evendlisteners. Added eventlisteners to bus and published the events

--- chapters/design-patterns/src/be/jochenhansoul/school/App.java
+++ chapters/design-patterns/src/be/jochenhansoul/school/App.java
@@ -1,4 +1,19 @@
 package be.jochenhansoul.school;
 
 public class App {
+    public static void main(String[] args) {
+        Event event = new ConcreteEvent(EventCategory.DEFCON_ONE, new Object());
+
+        EventBus bus = new EventBus();
+
+        EventListener listener1 = new ConcreteEventListener();
+        EventListener listener2 = new ConcreteEventListener();
+        EventListener listener3 = new ConcreteEventListener();
+
+        bus.addEventListener(listener1);
+        bus.addEventListener(listener2);
+        bus.addEventListener(listener3);
+
+        bus.publishEvent(event);
+    }
 }
