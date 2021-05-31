2d2afe6bf43ed31bc76abd724d2173f55abb9663
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 17:35:22 +0200

    created class EventBuss correctly and added counter, eventlistener propertys and methods to add, remove eventlisteners and a method to publish events

--- chapters/design-patterns/src/be/jochenhansoul/school/EventBus.java
+++ chapters/design-patterns/src/be/jochenhansoul/school/EventBus.java
@@ -1,4 +1,29 @@
 package be.jochenhansoul.school;
 
-public Interface EventBus {
+/*
+need listenerPool
+add/remove Listeners
+which Listener is interested in what
+    je kan de eventlistener verantwoordelijk maken of hij kan luisteren of ook de bus
+1. receove eve,ts
+2. send events
+*/
+
+public class EventBus {
+    private int counter = 0;
+    private EventListener[] eventListeners = new ConcreteEventListener[1];
+
+    public void addEventListener(EventListener listener) {
+        this.eventListeners[0] = listener;
+    }
+
+    public void removeEventListener(EventListener listener) {
+        // remove listener
+    }
+
+    public void publishEvent(Event event) {
+        for (EventListener eventListener : this.eventListeners) {
+            eventListener.onEvent(event);
+        }
+    }
 }
