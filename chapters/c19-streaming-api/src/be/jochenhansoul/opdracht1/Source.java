e93db05408f3d986efc08231c026d7684d6ab33e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 03:54:10 +0200

    added code for third part to create a stream out of a person array and print the values

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht1/Source.java
@@ -1,18 +1,32 @@
 package be.jochenhansoul.opdracht1;
 
+import java.util.Arrays;
 import java.util.stream.*;
 
 public class Source {
     public static void main(String[] args) {
         // part 1
+        System.out.println("part 1");
         String[] array1 = new String[]{"a", "b", "c", "d", "e"};
         Stream.of(array1)
-            .forEach(System.out::println);
+            .forEach((s) -> System.out.print(s + ", "));
+        System.out.println("\n\n");
 
         // part 2
+        System.out.println("part 2");
         IntStream.range(0, 100)
             .forEach((s) -> System.out.print(s + ", "));
+        System.out.println("\n\n");
 
         // part 3
+        System.out.println("part 3");
+        Person[] persons = new Person[] {
+            new Person("first1", "last1", Gender.MALE, 1, 50F, 1.7F),
+            new Person("first2", "last2", Gender.OTHER, 2, 50F, 1.7F),
+            new Person("first3", "last3", Gender.FEMALE, 3, 50F, 1.7F),
+            new Person("first3", "last3", Gender.UNKNOWN, 3, 50F, 1.7F)
+        };
+        Stream<Person> personStream = Arrays.stream(persons);
+        personStream.forEach(System.out::println);
     }
 }
