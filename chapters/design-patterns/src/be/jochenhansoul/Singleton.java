7803dc3ce0e6b4e5b4c83a2edba9ae121b10aaae
JochenHansoul <jochenhansoul101@hotmail.com>
2021-05-31 10:51:33 +0200

    put the code to prevent the creation of multiple objects into  a static method

--- chapters/design-patterns/src/be/jochenhansoul/Singleton.java
+++ chapters/design-patterns/src/be/jochenhansoul/Singleton.java
@@ -1,12 +1,27 @@
 package be.jochenhanoul;
 
+/*
+conditions:
+1. 1 instance only....
+final >> mag niet veranderen...
+wij >> geven de code het instance
+wij >> zet snoepjes(container) "constructor" waar de andere code er niet aankan
+    ENCAPSULATION
+2. 'eASY' access to said instance
+*/
+
 public class Singleton {
     private static boolean alreadyMade = false;
 
-    public Singleton() {
+    private Singleton() {
+    }
+
+    public static getInstance() {
         if (alreadyMade) {
             throw new Exception("only one instance of Singleton class allowed");
         } else {
+            return new Singleton();
             alreadyMade = true;
         }
+    }
 }
