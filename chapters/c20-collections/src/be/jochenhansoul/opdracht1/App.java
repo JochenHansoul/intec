0d569831df6bdfdaec4ed5066f5a98b1fe72d575
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-02 23:28:57 +0200

    added code that asks the user to enter numbers and then prints the numbers, the sum of the numbers and the average of the numbers

--- chapters/c20-collections/src/be/jochenhansoul/opdracht1/App.java
+++ chapters/c20-collections/src/be/jochenhansoul/opdracht1/App.java
@@ -1,4 +1,34 @@
 package be.jochenhansoul.opdracht1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many numbers do you want to enter?");
+        int amountOfNumbers = input.nextInt();
+        input.nextLine();
+
+        List<Integer> integers = new ArrayList<>();
+        for(int i = 0; i < amountOfNumbers; i++) {
+            System.out.println("enter number");
+            integers.add(input.nextInt());
+            input.nextLine();
+        }
+        int sum = integers.stream()
+            //.reduce(0, (a, b) -> a + b);
+            .reduce(0, Integer::sum);
+
+        // print numbers
+        System.out.println(integers);
+
+        // print sum
+        System.out.println(sum);
+        // print average
+        System.out.println(sum / integers.size());
+        input.close();
+    }
 }
