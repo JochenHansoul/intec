ca92090087b7539881c13ecdce3e18700cf56dcf
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-05 20:03:06 +0200

    simplified some lambda expressions in c18-lambdas App class

--- chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/App.java
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht1/App.java
@@ -16,18 +16,12 @@ public class App {
         tp.printFilteredWords((s) -> s.length() > 4);
 
         System.out.println("***words starting with letter 'a'***");
-        tp.printFilteredWords((s) -> s.substring(0, 1).equals("a"));
+        tp.printFilteredWords((s) -> s.charAt(0) == 'a');
 
         System.out.println("***words with second letter 'e'***");
-        tp.printFilteredWords((s) -> (s.length() > 1) ? s.substring(1, 2).equals("e") : false);
+        tp.printFilteredWords((s) -> s.length() > 1 && s.charAt(1) == 'e');
 
         System.out.println("***words that contain two letters 'e'***");
-        tp.printFilteredWords((s) -> {
-            return s.contains("e") ? s.substring(s.indexOf('e') + 1).contains("e") : false;
-        });
-    }
-
-    private static String deleteCharAt(String s, int index) {
-        return s.substring(0, index) + s.substring(index + 1);
+        tp.printFilteredWords((s) -> s.contains("e") && s.substring(s.indexOf('e') + 1).contains("e"));
     }
 }
