51ad128afcaf0f8aa06c9dad7394cc24785b9c5f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-06 18:57:05 +0200

    Added second constructor to TextPrinter class (package oefening3) that accepts a consumer functional interface

--- chapters/c18-lambdas/src/be/jochenhansoul/opdracht3/TextPrinter.java
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht3/TextPrinter.java
@@ -1,33 +1,40 @@
 package be.jochenhansoul.opdracht3;
 
 import java.math.BigDecimal;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class TextPrinter {
-    private String sentence;
+    private final String sentence;
+    private final Consumer<String> consumer;
 
     public TextPrinter(String sentence) {
+        this(sentence, System.out::print);
+    }
+
+    public TextPrinter(String sentence, Consumer<String> consumer) {
         this.sentence = sentence;
+        this.consumer = consumer;
     }
 
     public void printFilteredWords(Predicate<String> predicate) {
         for (String w : sentence.split(" ")) {
             if (predicate.test(w)) {
-                System.out.println(w);
+                this.consumer.accept(w);
             }
         }
     }
 
     public void printProcessedWords(Function<String, String> processor) {
         for (String w : sentence.split(" ")) {
-            System.out.println(processor.apply(w));
+            consumer.accept(processor.apply(w));
         }
     }
 
     public void printNumberValues(Function<String, BigDecimal> parser) {
         for (String w : sentence.split(" ")) {
-            System.out.println(parser.apply(w));
+            consumer.accept(parser.apply(w).toString());
         }
     }
 
