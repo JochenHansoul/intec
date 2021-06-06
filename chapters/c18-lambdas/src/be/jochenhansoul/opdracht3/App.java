dfb0a7b318836bcfae6f6abdce4e353080836ad3
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-06 18:59:23 +0200

    Added code that used the default predicate functional interface and added code that reversed the words using the default converter  functional interface

--- chapters/c18-lambdas/src/be/jochenhansoul/opdracht3/App.java
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht3/App.java
@@ -6,6 +6,10 @@ public class App {
     public static void main(String[] args) {
         String hello = "Hello, is it me you're looking for";
 
+        System.out.println("predicate:");
+        TextPrinter tp = new TextPrinter("water is wet");
+        tp.printFilteredWords((s) -> s.contains("a") && s.contains("e"));
+
         System.out.println("\nUnbound method reference:");
         TextPrinter tp1 = new TextPrinter(hello);
         tp1.printProcessedWords(String::toLowerCase);
@@ -16,5 +20,11 @@ public class App {
 
         System.out.println("\nPrint sum numbers");
         tp2.printSum(BigDecimal::new);
+
+        System.out.println("\nprint reversed words:");
+        TextPrinter tp3 = new TextPrinter(
+                "this is a sentence with reversed words",
+                (s) -> System.out.println("reversed: " + s));
+        tp3.printProcessedWords((s) -> new StringBuilder(s).reverse().toString().toUpperCase());
     }
 }
