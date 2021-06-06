d61387cdaaeca42fdec2be2723d24d80c6f6ea5b
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-06 16:08:13 +0200

    Created a new TextPrinter class in module opdracht3 and changed all the functional interfaces to standard functional interfaces

---
+++ chapters/c18-lambdas/src/be/jochenhansoul/opdracht3/TextPrinter.java
@@ -0,0 +1,41 @@
+package be.jochenhansoul.opdracht3;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class TextPrinter {
+    private String sentence;
+
+    public TextPrinter(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public void printFilteredWords(Predicate<String> predicate) {
+        for (String w : sentence.split(" ")) {
+            if (predicate.test(w)) {
+                System.out.println(w);
+            }
+        }
+    }
+
+    public void printProcessedWords(Function<String, String> processor) {
+        for (String w : sentence.split(" ")) {
+            System.out.println(processor.apply(w));
+        }
+    }
+
+    public void printNumberValues(Function<String, Integer> parser) {
+        for (String w : sentence.split(" ")) {
+            System.out.println(parser.apply(w));
+        }
+    }
+
+    public void printSum(Function<String, BigDecimal> parser) {
+        BigDecimal bigDecimal = BigDecimal.ZERO;
+        for (String w : sentence.split(" ")) {
+            bigDecimal = bigDecimal.add(parser.apply(w));
+        }
+        System.out.println(bigDecimal);
+    }
+}
