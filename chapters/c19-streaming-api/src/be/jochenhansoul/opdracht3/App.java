f45c183d8298a16ea0eaf54b8f3a0287949089fe
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-11 23:10:35 +0200

    Added code to transform the person array into stream and back into a new array.

--- chapters/c19-streaming-api/src/be/jochenhansoul/opdracht3/App.java
+++ chapters/c19-streaming-api/src/be/jochenhansoul/opdracht3/App.java
@@ -2,6 +2,9 @@ package be.jochenhansoul.opdracht3;
 
 import be.jochenhansoul.general.*;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
         Person[] persons = new Person[] {
@@ -10,5 +13,16 @@ public class App {
             new Person("first3", "last3", Gender.FEMALE, 3, 50F, 1.7F),
             new Person("first3", "last3", Gender.UNKNOWN, 3, 50F, 1.7F)
         };
+
+        Person[] newPersonArray = Arrays.stream(persons)
+                .toArray(Person[]::new);
+
+        for (Person person : newPersonArray) {
+            System.out.println(person);
+        }
+
+        // just a stream test
+        Stream<String> stream = Stream.generate(() -> "this is a string");
+        stream.limit(1).forEach(System.out::println);
     }
 }
