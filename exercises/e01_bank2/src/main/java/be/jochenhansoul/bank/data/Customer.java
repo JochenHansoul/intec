61b9104ecadf7721302c4ae375a2cf90864b22e4
JochenHansoul <jochenhansoul101@hotmail.com>
2021-06-13 22:33:45 +0200

    Refactored alignment code, put arguments under each other and put space between arguments and code block

--- exercises/e01_bank2/src/main/java/be/jochenhansoul/bank/data/Customer.java
+++ exercises/e01_bank2/src/main/java/be/jochenhansoul/bank/data/Customer.java
@@ -4,11 +4,24 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Customer extends Person {
+    public Customer(
+        int id,
+        String firstName,
+        String lastName,
+        Address address,
+        LocalDate birthDate) {
 
-    public Customer(int id, String firstName, String lastName, Address address, LocalDate birthDate) {
-        this(id, firstName, lastName, address, birthDate, new ArrayList());
+        this(id, firstName, lastName, address, birthDate, new ArrayList<>());
     }
-    public Customer(int id, String firstName, String lastName, Address address, LocalDate birthDate, ArrayList<ContactInfo> contactInfos) {
+
+    public Customer(
+        int id,
+        String firstName,
+        String lastName,
+        Address address,
+        LocalDate birthDate,
+        ArrayList<ContactInfo> contactInfos) {
+
         super(id, firstName, lastName, address, birthDate, contactInfos);
     }
 }
