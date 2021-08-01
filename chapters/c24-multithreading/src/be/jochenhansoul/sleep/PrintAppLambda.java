afc611c3428a5c7f10f05dc3fa7887a9323252f5
JochenHansoulIntec <jochen.hansoul@student.intecbrussel.be>
2021-08-01 03:02:57 +0200

    Added the PrintAppLambda excercise to test the joind() method in Threads.

---
+++ chapters/c24-multithreading/src/be/jochenhansoul/sleep/PrintAppLambda.java
@@ -0,0 +1,30 @@
+package be.jochenhansoul.sleep;
+
+public class PrintAppLambda {
+    public static void main(String[] args)
+        throws InterruptedException {
+        Thread thread1 = new Thread(() -> print('#', 50));
+        Thread thread2 = new Thread(() -> print('*', 50));
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("End");
+    }
+
+    public static void print(char c, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(c);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
