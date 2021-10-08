6a6edc5e3f6051aab117707ca51db9b39c1c1643
JochenHansoul <jochenhansoul101@hotmail.com>
2021-10-08 18:12:55 +0200

    Created a person object en tryed adding it to the persistence contex

--- jpa/demo/01-jpa/src/main/java/be/jochenhansoul/App.java
+++ jpa/demo/01-jpa/src/main/java/be/jochenhansoul/App.java
@@ -1,7 +1,28 @@
 package be.jochenhansoul;
 
+import be.jochenhansoul.data.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class App {
-    public static void main(String[] arg) {
-        System.out.println("hello world");
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceJochen");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction et = em.getTransaction();
+        Person person = new Person();
+        person.setPersonId(1);
+        person.setFirstName("first1");
+        person.setLastname("last1");
+        person.setBirthDate(LocalDate.of(2001, 1 , 1));
+
+        et.begin();
+        em.persist(person);
+        et.commit();
     }
 }
