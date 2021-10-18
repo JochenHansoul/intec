1b9aa669a1cdeba077bcc52ee9dafdad6b2aa3db
JochenHansoul <jochenhansoul101@hotmail.com>
2021-10-18 15:18:37 +0200

    Added function te remove all the child elements

--- javascript/1-intro/oef/1-todo/js/index.js
+++ javascript/1-intro/oef/1-todo/js/index.js
@@ -2,4 +2,11 @@
 
 const H1_TITLE = document.getElementById("js-title");
 
+function removeAllChildren(element) {
+  while (element.hasChildNodes()) {
+    element.lastChild.remove();
+  }
+}
+
+removeAllChildren(H1_TITLE);
 H1_TITLE.appendChild(document.createTextNode("hello, world"));
