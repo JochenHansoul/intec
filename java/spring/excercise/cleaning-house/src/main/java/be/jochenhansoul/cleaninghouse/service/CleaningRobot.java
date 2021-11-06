cd29dbd9b75ce6440795a48065e8efe5c0fc6646
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-06 22:50:34 +0100

    Implemented CleaningServiceable interface into CleaningRobot and created method and added a cleaniingTools dependancy injection

--- java/spring/excercise/cleaning-house/src/main/java/be/jochenhansoul/cleaninghouse/service/CleaningRobot.java
+++ java/spring/excercise/cleaning-house/src/main/java/be/jochenhansoul/cleaninghouse/service/CleaningRobot.java
@@ -2,9 +2,26 @@ package be.jochenhansoul.cleaninghouse.service;
 
 import be.jochenhansoul.cleaninghouse.service.tool.*;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
-public class CleaningRobot {
-    private List<CleaningTool> cleaningTools = Arrays.asList(new Broom(), new VacuumCleaner(), new Swiffer());
+public class CleaningRobot implements CleanServiceable {
+    private final Random RAND = new Random(5);
+
+    private List<CleaningTool> cleaningTools; // = Arrays.asList(new Broom(), new VacuumCleaner(), new Swiffer());
+
+    public CleaningRobot(List<CleaningTool> cleaningTools) {
+        this.cleaningTools = cleaningTools;
+    }
+
+    @Override
+    public void clean() {
+        if (this.RAND.nextInt(2) == 0) {
+            this.cleaningTools.get(this.RAND.nextInt(this.cleaningTools.size())).use();
+        } else {
+            for (CleaningTool tool : this.cleaningTools) {
+                tool.use();
+            }
+        }
+    }
 }
