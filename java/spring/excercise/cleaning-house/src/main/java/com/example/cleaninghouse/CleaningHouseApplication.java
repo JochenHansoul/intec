9e7ddaeb1a5107ff9bace45f018a05b1f72a2a1a
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-06 13:01:09 +0100

    Created start of cleaning-house project

---
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/CleaningHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleaninghouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningHouseApplication.class, args);
+    }
+
+}
