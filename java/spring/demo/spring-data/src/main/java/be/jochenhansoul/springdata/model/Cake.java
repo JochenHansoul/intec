b0eb91738257ccf0c600e8f10c97ebad5ec46160
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-16 00:05:31 +0100

    Filled in methdos amd properties of Cake

--- java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/model/Cake.java
+++ java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/model/Cake.java
@@ -1,2 +1,40 @@
-package be.jochenhansoul.springdata.model;public class Cake {
+package be.jochenhansoul.springdata.model;
+
+public class Cake {
+    private Integer size;
+    private String name;
+    private Boolean hasChocolate;
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHasChocolate(Boolean hasChocolate) {
+        this.hasChocolate = hasChocolate;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean getHasChocolate() {
+        return hasChocolate;
+    }
+
+    @Override
+    public String toString() {
+        return "Cake{" +
+                "size=" + size +
+                ", name='" + name + '\'' +
+                ", hasChocolate=" + hasChocolate +
+                '}';
+    }
 }
