9a5d4d0dacb4d226deb599d64eefe654f84b5db3
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-15 10:18:44 +0100

    Added RequestMapping annotation to class

--- java/spring/demo/rest/src/main/java/be/jochenhansoul/rest/controller/RestController.java
+++ java/spring/demo/rest/src/main/java/be/jochenhansoul/rest/controller/RestController.java
@@ -3,16 +3,17 @@ package be.jochenhansoul.rest.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("api/")
 public class RestController {
 
     //localhost:8080/hello
-    @RequestMapping("/hello")
+    @RequestMapping("hello")
     public String helloWorld() {
         System.out.println("hello world method called");
         return "hello world";
     }
 
-    @RequestMapping("/goodbye")
+    @RequestMapping("goodbye")
     public String goodbye() {
         return "goodbye";
     }
