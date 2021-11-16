1c6a36f8a28fd329a92fe0c5b2b7dcc56de08fc2
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-16 21:27:09 +0100

    Exteded CakeRepository from JpaRepository

--- java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/repository/CakeRepository.java
+++ java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/repository/CakeRepository.java
@@ -1,2 +1,7 @@
-package be.jochenhansoul.springdata.repository;public interface CakeRepository {
+package be.jochenhansoul.springdata.repository;
+
+import be.jochenhansoul.springdata.model.Cake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CakeRepository extends JpaRepository<Cake, Integer> {
 }
