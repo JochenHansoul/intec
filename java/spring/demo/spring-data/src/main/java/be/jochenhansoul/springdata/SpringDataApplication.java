d43698ed4dee262e373f1e74bc0f86037feacff5
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-16 21:48:13 +0100

    Created a cake and added it to the applicationcontext

--- java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/SpringDataApplication.java
+++ java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/SpringDataApplication.java
@@ -1,13 +1,27 @@
 package be.jochenhansoul.springdata;
 
+import be.jochenhansoul.springdata.model.Cake;
+import be.jochenhansoul.springdata.repository.CakeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringDataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringDataApplication.class, args);
+
+		CakeRepository repository = ctx.getBean(CakeRepository.class);
+		System.out.println(repository);
+
+		Cake cake = new Cake();
+		cake.setName("chocolate cake");
+		cake.setSize(10);
+		cake.setHasChocolate(true);
+
+		Cake savedCake = repository.save(cake);
+		System.out.println("saved cake: " + savedCake);
 	}
 
 }
