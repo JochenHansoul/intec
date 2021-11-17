b14a5e65c855d1aa3de8b97260986dd30bfd1e22
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-17 11:40:04 +0100

    Added a constructor to Cake class en replaced @Data with methods and @Getter and @Setter annotation

--- java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/model/Cake.java
+++ java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/model/Cake.java
@@ -1,18 +1,43 @@
 package be.jochenhansoul.springdata.model;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Cake {
     @Id
     @GeneratedValue
     private Integer id;
-    private Integer size;
     private String name;
+    private Integer size;
     private Boolean hasChocolate;
+
+    public Cake(String name, Integer size, Boolean hasChocolate) {
+        this.name = name;
+        this.size = size;
+        this.hasChocolate = hasChocolate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Cake cake = (Cake) o;
+        return id != null && Objects.equals(id, cake.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
