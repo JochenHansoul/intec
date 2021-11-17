4d7a0b6e9289f9bd15376c02d76d37afdb4c7beb
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-17 12:32:29 +0100

    Added a Query to find cakes with the same name

--- java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/repository/CakeRepository.java
+++ java/spring/demo/spring-data/src/main/java/be/jochenhansoul/springdata/repository/CakeRepository.java
@@ -2,9 +2,13 @@ package be.jochenhansoul.springdata.repository;
 
 import be.jochenhansoul.springdata.model.Cake;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CakeRepository extends JpaRepository<Cake, Integer> {
     List<Cake> findCakeBySize(Integer size);
+
+    @Query("SELECT c FROM Cake c WHERE c.name LIKE :name")
+    List<Cake> findSomeCake(String name);
 }
