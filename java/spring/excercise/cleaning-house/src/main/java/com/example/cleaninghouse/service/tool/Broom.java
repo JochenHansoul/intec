130aa6c67de83d9a00888de744d2b787305f59c8
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 00:28:32 +0100

    Added code to output specific languages in Broom class

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Broom.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Broom.java
@@ -1,8 +1,20 @@
 package com.example.cleaninghouse.service.tool;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Broom extends CleaningTool {
+    @Value("${language}")
+    String language;
+    @Value("${country}")
+    String country;
+    Locale locale = new Locale(language, country);
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("res.bundle", locale);
+
     @Override
     public void use() {
-        System.out.println("using broom");
+        System.out.println(resourceBundle.getString("tool.broom.use"));
     }
 }
