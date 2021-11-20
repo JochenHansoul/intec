d382a11335d2826bbc0d191d49da4c5bd7497c2e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 13:04:28 +0100

    Adjusted VacuumCleaner class to work with multiple languages

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/VacuumCleaner.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/VacuumCleaner.java
@@ -1,8 +1,16 @@
 package com.example.cleaninghouse.service.tool;
 
+import java.util.ResourceBundle;
+
 public class VacuumCleaner extends CleaningTool {
+    ResourceBundle resourceBundle;
+
+    public VacuumCleaner(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @Override
     public void use() {
-        System.out.println("usiing vacuum cleaner");
+        System.out.println(resourceBundle.getString("tool.vacuum.cleaner.use"));
     }
 }
