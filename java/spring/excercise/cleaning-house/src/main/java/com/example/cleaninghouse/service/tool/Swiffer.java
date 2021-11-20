7c0ae844ac1664ef4b5c94845e6deecc7d006d5f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 13:02:56 +0100

    Adjusted Swiffer class to work with multiple languages

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Swiffer.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Swiffer.java
@@ -1,8 +1,16 @@
 package com.example.cleaninghouse.service.tool;
 
+import java.util.ResourceBundle;
+
 public class Swiffer extends CleaningTool {
+    ResourceBundle resourceBundle;
+
+    public Swiffer(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @Override
     public void use() {
-        System.out.println("using swiffer");
+        System.out.println(resourceBundle.getString("tool.swiffer.use"));
     }
 }
