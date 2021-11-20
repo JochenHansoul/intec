fce94ab10b77aead8f591c36ebc24217263e8e3a
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 13:05:56 +0100

    Adjusted LawnMower class to work with multiple languages

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/LawnMower.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/LawnMower.java
@@ -1,8 +1,16 @@
 package com.example.cleaninghouse.service.tool;
 
+import java.util.ResourceBundle;
+
 public class LawnMower extends GardeningTool {
+    ResourceBundle resourceBundle;
+
+    public LawnMower(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @Override
     public void use() {
-        System.out.println("using lawnmower");
+        System.out.println(resourceBundle.getString("tool.lawnmower.use"));
     }
 }
