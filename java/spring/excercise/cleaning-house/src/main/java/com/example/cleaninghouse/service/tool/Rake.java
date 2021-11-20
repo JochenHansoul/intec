662eec1c6e27b237cf72e70447e7ca6ff55f1e8c
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 13:06:35 +0100

    Adjusted Rake class to work with multiple languages

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Rake.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Rake.java
@@ -1,8 +1,16 @@
 package com.example.cleaninghouse.service.tool;
 
+import java.util.ResourceBundle;
+
 public class Rake extends GardeningTool {
+    ResourceBundle resourceBundle;
+
+    public Rake(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @Override
     public void use() {
-        System.out.println("using rake");
+        System.out.println(resourceBundle.getString("tool.rake.use"));
     }
 }
