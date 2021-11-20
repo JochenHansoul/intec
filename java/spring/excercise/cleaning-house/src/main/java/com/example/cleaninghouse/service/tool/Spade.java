c029dec570fa7c88f5374f50163b4cbd6e8f422a
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-20 13:07:25 +0100

    Adjusted Spade class to work with multiple languages

--- java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Spade.java
+++ java/spring/excercise/cleaning-house/src/main/java/com/example/cleaninghouse/service/tool/Spade.java
@@ -1,8 +1,16 @@
 package com.example.cleaninghouse.service.tool;
 
+import java.util.ResourceBundle;
+
 public class Spade extends GardeningTool {
+    ResourceBundle resourceBundle;
+
+    public Spade(ResourceBundle resourceBundle) {
+        this.resourceBundle = resourceBundle;
+    }
+
     @Override
     public void use() {
-        System.out.println("using spade");
+        System.out.println(resourceBundle.getString("tool.spade.use"));
     }
 }
