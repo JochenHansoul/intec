2c62d2460550fa68a5f67f455d200dfc821b463f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-23 23:50:56 +0100

    Filled in all properties, constructor, aquels method and annotations for Category class

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Category.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Category.java
@@ -1,4 +1,41 @@
 package be.jochenhansoul.petstore.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@RequiredArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id.equals(category.id) && name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
