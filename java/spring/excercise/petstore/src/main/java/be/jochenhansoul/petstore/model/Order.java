f5939141fa8104ade02d81c8c8dcdcb5783e25ed
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-23 23:38:14 +0100

    Filled in Order class properties, constructors, equals methods and annotations

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Order.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Order.java
@@ -1,4 +1,49 @@
 package be.jochenhansoul.petstore.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long petId;
+    private Integer quantity;
+    private LocalDateTime shipDate;
+    private OrderStatus status;
+    private Boolean complete = false;
+
+    public Order(Long id, Long petId, Integer quantity, LocalDateTime shipDate, OrderStatus status) {
+        this.id = id;
+        this.petId = petId;
+        this.quantity = quantity;
+        this.shipDate = shipDate;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id) && petId.equals(order.petId) && quantity.equals(order.quantity) && shipDate.equals(order.shipDate) && status == order.status && complete.equals(order.complete);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, petId, quantity, shipDate, status, complete);
+    }
 }
