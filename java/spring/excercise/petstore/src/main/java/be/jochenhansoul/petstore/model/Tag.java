16acdc27347aea35a9787627fb6100ea4200680f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 00:08:55 +0100

    Filled in Tag class with properties, methods and annotations

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Tag.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Tag.java
@@ -1,4 +1,34 @@
 package be.jochenhansoul.petstore.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return id.equals(tag.id) && name.equals(tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
