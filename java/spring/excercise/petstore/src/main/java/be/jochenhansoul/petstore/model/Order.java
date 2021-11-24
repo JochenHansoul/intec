dc174eee5faf980f3284ba96c5096ecf648c988e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 14:03:41 +0100

    Changed petId to pet (Pet object) in Order class

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Order.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/Order.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -20,15 +21,16 @@ public class Order {
     @Id
     @GeneratedValue
     private Long id;
-    private Long petId;
+    @ManyToOne
+    private Pet pet;
     private Integer quantity;
     private LocalDateTime shipDate;
     private OrderStatus status;
     private Boolean complete = false;
 
-    public Order(Long id, Long petId, Integer quantity, LocalDateTime shipDate, OrderStatus status) {
+    public Order(Long id, Pet pet, Integer quantity, LocalDateTime shipDate, OrderStatus status) {
         this.id = id;
-        this.petId = petId;
+        this.pet = pet;
         this.quantity = quantity;
         this.shipDate = shipDate;
         this.status = status;
@@ -39,11 +41,11 @@ public class Order {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        return id.equals(order.id) && petId.equals(order.petId) && quantity.equals(order.quantity) && shipDate.equals(order.shipDate) && status == order.status && complete.equals(order.complete);
+        return id.equals(order.id) && pet.equals(order.pet) && quantity.equals(order.quantity) && shipDate.equals(order.shipDate) && status == order.status && complete.equals(order.complete);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, petId, quantity, shipDate, status, complete);
+        return Objects.hash(id, pet, quantity, shipDate, status, complete);
     }
 }
