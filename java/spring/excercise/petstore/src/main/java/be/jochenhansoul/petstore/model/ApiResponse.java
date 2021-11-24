664e4a1c9ae84077e4f6522f6ee70acf3f5edf1a
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 01:05:47 +0100

    Filled in ApiRespone properties, methods and annotations

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/ApiResponse.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/model/ApiResponse.java
@@ -1,4 +1,35 @@
 package be.jochenhansoul.petstore.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Setter
+@Getter
+@ToString
 public class ApiResponse {
+    @Id
+    @GeneratedValue
+    private Integer code;
+    private String type;
+    private String message;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ApiResponse that = (ApiResponse) o;
+        return code.equals(that.code) && type.equals(that.type) && message.equals(that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, type, message);
+    }
 }
