9f7868b1aa9b136cce5df9b3ef9f2822d84b5cae
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 19:23:47 +0100

    added methods to get a pet by id, delete a pet by id and get a list of pets based on the status

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
@@ -1,2 +1,15 @@
-package be.jochenhansoul.petstore.repository;public interface PetRepository {
+package be.jochenhansoul.petstore.repository;
+
+import be.jochenhansoul.petstore.model.Pet;
+import be.jochenhansoul.petstore.model.PetStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    Pet getById(Long id);
+
+    void deleteById(Long id);
+
+    List<Pet> getPetByStatus(PetStatus status);
 }
