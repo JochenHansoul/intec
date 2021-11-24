ba012515f8af767624dee7c7eb415e22b19ac243
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 21:59:26 +0100

    Created static method validate

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/service/PetValidator.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/service/PetValidator.java
@@ -1,4 +1,13 @@
 package be.jochenhansoul.petstore.service;
 
+import be.jochenhansoul.petstore.model.Pet;
+
 public class PetValidator {
+    public static boolean validate(Pet pet) {
+        return pet.getId() == 0
+                && pet.getCategory() != null
+                && pet.getName() != null
+                && pet.getName().length() > 0
+                && pet.getStatus() != null;
+    }
 }
