e1a7b8b549fdb1ea75254834aa75794fdbae292f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-24 19:34:05 +0100

    Added getById and deleteById methods in PetOrderRepository

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetOrderRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetOrderRepository.java
@@ -1,2 +1,10 @@
-package be.jochenhansoul.petstore.repository;public interface PetOrderRepository {
+package be.jochenhansoul.petstore.repository;
+
+import be.jochenhansoul.petstore.model.PetOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetOrderRepository extends JpaRepository<PetOrder, Long> {
+    PetOrder getById(Long id);
+
+    void deleteById(Long id);
 }
