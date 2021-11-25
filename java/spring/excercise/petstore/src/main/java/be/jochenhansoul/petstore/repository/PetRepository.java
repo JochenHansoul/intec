fb39f68ea36811daf53b8b6be989eb88bcfc30a2
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-25 01:37:02 +0100

    Added updatePet method

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
@@ -3,6 +3,7 @@ package be.jochenhansoul.petstore.repository;
 import be.jochenhansoul.petstore.model.Pet;
 import be.jochenhansoul.petstore.model.PetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +13,7 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
     void deleteById(Long id);
 
     List<Pet> getPetsByStatus(PetStatus status);
+
+    @Query("UPDATE Pet p SET p.name = :name, p.status = :status WHERE p.id = :id")
+    Pet updatePet(Long id, String name, PetStatus status);
 }
