74550582d4bcf131202bc507039a5788cb7df016
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-25 02:07:31 +0100

    Added nativequery to update the pet photo urls

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
@@ -16,4 +16,8 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
 
     @Query("UPDATE Pet p SET p.name = :name, p.status = :status WHERE p.id = :id")
     Pet updatePet(Long id, String name, PetStatus status);
+
+    // native query
+    @Query(value = "INSERT INTO pet_photo_urls VALUE(:id, :url)", nativeQuery = true)
+    void addPhotoUrl(Long id, String url);
 }
