c9225b663ffa6508a99df2f6e636794ad773bc1f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-25 03:50:06 +0100

    Added getPet(Long id) method

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/controller/PetController.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/controller/PetController.java
@@ -1,4 +1,34 @@
 package be.jochenhansoul.petstore.controller;
 
+import be.jochenhansoul.petstore.model.Pet;
+import be.jochenhansoul.petstore.service.PetService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("pet/")
 public class PetController {
+    private final PetService PET_SERVICE;
+
+    public PetController(PetService petService) {
+        this.PET_SERVICE = petService;
+    }
+
+    @PostMapping
+    public ResponseEntity addPet(@RequestBody Pet pet) {
+        Optional<Pet> optionalPet = PET_SERVICE.save(pet);
+        return (optionalPet.isPresent()) ? ResponseEntity.ok("found") : ResponseEntity.status(405).build();
+    }
+
+    @GetMapping
+    public ResponseEntity getPet(@RequestBody Long id) {
+        if (id <= 0) {
+            return ResponseEntity.status(400).build();
+        } else {
+            Optional<Pet> optionalPet = PET_SERVICE.getPet(id);
+            return (optionalPet.isPresent()) ? ResponseEntity.ok(optionalPet.get()) : ResponseEntity.status(404).build();
+        }
+    }
 }
