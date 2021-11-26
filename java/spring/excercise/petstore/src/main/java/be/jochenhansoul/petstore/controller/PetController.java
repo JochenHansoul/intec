e181a5a2755a62b365b31aaa5e3a24a86385f7d7
JochenHansoul <jochenhansoul101@hotmail.com>
2021-11-26 12:51:46 +0100

    Fixed getPet and deletePet method (added {petId} with their annotation, changed @RequestBody to @PathVariable and changed parameter to type String

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/controller/PetController.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/controller/PetController.java
@@ -22,22 +22,34 @@ public class PetController {
         return (optionalPet.isPresent()) ? ResponseEntity.ok("found") : ResponseEntity.status(405).build();
     }
 
-    @GetMapping
-    public ResponseEntity getPet(@RequestBody Long id) {
-        if (id <= 0) {
-            return ResponseEntity.status(400).build();
+    @GetMapping("{petId}")
+    public ResponseEntity getPet(@PathVariable String petId) {
+        Long id = null;
+        try {
+            id = Long.parseLong(petId);
+        } catch (NumberFormatException ignored) {
+        }
+        if (id == null) {
+            return ResponseEntity.status(400).body("id is not of type number");
+        } else if (id <= 0) {
+            return ResponseEntity.status(400).body("id may not be lower then zero");
         } else {
             Optional<Pet> optionalPet = PET_SERVICE.getPet(id);
-            return (optionalPet.isPresent())
-                    ? ResponseEntity.ok(optionalPet.get())
-                    : ResponseEntity.status(404).build();
+            return (optionalPet.isPresent()) ? ResponseEntity.ok(optionalPet.get())
+                    : ResponseEntity.status(404).body("No pet found with id " + id);
         }
     }
 
-    @DeleteMapping
-    public ResponseEntity deletePet(@RequestBody Long id) {
-        return (id <= 0) ? ResponseEntity.status(400).build() :
-                (PET_SERVICE.delete(id)) ? ResponseEntity.ok(200)
-                        : ResponseEntity.status(404).build();
+    @DeleteMapping("{petId}")
+    public ResponseEntity deletePet(@PathVariable String petId) {
+        Long id = null;
+        try {
+            id = Long.parseLong(petId);
+        } catch (NumberFormatException ignored) {
+        }
+        return (id == null) ? ResponseEntity.status(400).body("id is not of type number") :
+                (id <= 0) ? ResponseEntity.status(400).body("id may not be lower then zero") :
+                        (PET_SERVICE.delete(id)) ? ResponseEntity.ok(200)
+                                : ResponseEntity.status(404).body("No pet found with id " + id);
     }
 }
