56e43246ee3dc0721738cac07230468312a589b5
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-16 20:11:56 +0100

    Created the Spring back-end app

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package be.jochenhansoul.yummieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummieAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummieAppBackEndApplication.class, args);
+    }
+
+}
---
+++ yummie-app/yummie-app-back-end/src/test/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jochenhansoul.yummieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummieAppBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
