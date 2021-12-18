a054cd9fec602f870f1dee29ebb972eb14746729
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 17:55:18 +0100

    Added @Entity annotation and id

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/FoodCategory.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/FoodCategory.java
@@ -1,5 +1,20 @@
 package be.jochenhansoul.yummieapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public enum FoodCategory {
-    CAFE, CAFETERIA, COFFEE, BISTRO, RESTAURANT, PIZZA, HAMBURGER, HOT_DOG, KEBAB, MEXICAN, CHINESE, INDIAN, THAI, FOOD_TRUCK
+    CAFE, CAFETERIA, COFFEE, BISTRO, RESTAURANT, PIZZA, HAMBURGER, HOT_DOG, KEBAB, MEXICAN, CHINESE, INDIAN, THAI, FOOD_TRUCK;
+
+    @Id
+    private Long idFoodCategory;
+
+    public Long getIdFoodCategory() {
+        return idFoodCategory;
+    }
+
+    public void setIdFoodCategory(Long idFoodCategory) {
+        this.idFoodCategory = idFoodCategory;
+    }
 }
