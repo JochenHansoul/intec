cb920cb3bef0962e6754e680a9e297cea6294acb
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 01:55:44 +0100

    Created Address class

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/Address.java
@@ -0,0 +1,24 @@
+package be.jochenhansoul.yummieapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@Accessors(chain = true)
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idAddress;
+    private String country;
+    private String zipCode;
+    private String street;
+    private String number;
+}
