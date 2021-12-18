4fdcbedd5b53720f7bb5d28a114c2381807b5df4
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 17:31:49 +0100

    Created MomentOpen class

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/MomentOpen.java
@@ -0,0 +1,23 @@
+package be.jochenhansoul.yummieapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalTime;
+
+@Entity
+@Setter
+@Getter
+@Accessors(chain = true)
+public class MomentOpen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private LocalTime open;
+    private LocalTime closed;
+}
