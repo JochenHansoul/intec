d58e42569ef73df39134b511ab1a87a7dabf5bb3
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 03:21:04 +0100

    Created enum FoodCategory

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/FoodCategory.java
@@ -0,0 +1,5 @@
+package be.jochenhansoul.yummieapp.model;
+
+public enum FoodCategory {
+    CAFE, CAFETERIA, COFFEE, BISTRO, RESTAURANT, PIZZA, HAMBURGER, HOT_DOG, KEBAB, MEXICAN, CHINESE, INDIAN, THAI, FOOD_TRUCK
+}
