732ba8d75dd8f8e2357a78e21e9c8da93d9e76de
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 03:23:19 +0100

    Created class Allergen

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/Allergen.java
@@ -0,0 +1,21 @@
+package be.jochenhansoul.yummieapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@Accessors(chain = true)
+public class Allergen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idAllergen;
+    private String name;
+}
