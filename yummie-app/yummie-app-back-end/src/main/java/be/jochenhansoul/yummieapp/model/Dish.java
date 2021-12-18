2663b9605144a0300f2b31ee55133d0e2bc4132e
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 03:27:53 +0100

    Created class Dish

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/Dish.java
@@ -0,0 +1,23 @@
+package be.jochenhansoul.yummieapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@Accessors(chain = true)
+public class Dish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idDish;
+    private String name;
+    private String pictureUrl;
+    private float price;
+    @OneToMany
+    private List<Allergen> allergenList;
+}
