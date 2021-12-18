e45da4f07a60cae4b17d8453007579614f7c473f
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-18 01:39:46 +0100

    Created TelephoneNumber class

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/TelephoneNumber.java
@@ -0,0 +1,20 @@
+package be.jochenhansoul.yummieapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+public class TelephoneNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idTelephoneNumber;
+    private String description;
+    private long number;
+}
