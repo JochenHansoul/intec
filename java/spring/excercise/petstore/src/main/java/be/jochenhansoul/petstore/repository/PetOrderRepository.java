af50e08816a26a6eff7d6b597ca7ff7df3e453e7
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-20 17:29:48 +0100

    Added query to get a PetOrder based on id

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetOrderRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetOrderRepository.java
@@ -2,8 +2,10 @@ package be.jochenhansoul.petstore.repository;
 
 import be.jochenhansoul.petstore.model.PetOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PetOrderRepository extends JpaRepository<PetOrder, Long> {
+    @Query("SELECT p FROM PetOrder p WHERE p.id = :id")
     PetOrder getById(Long id);
 
     void deleteById(Long id);
