7bf94396b8e0ddc26df2c26580dc330d9ae1cb77
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-20 03:40:13 +0100

    Added Modifier annotation and changed incorrect return type to int

--- java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
+++ java/spring/excercise/petstore/src/main/java/be/jochenhansoul/petstore/repository/PetRepository.java
@@ -3,6 +3,7 @@ package be.jochenhansoul.petstore.repository;
 import be.jochenhansoul.petstore.model.Pet;
 import be.jochenhansoul.petstore.model.PetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -15,13 +16,17 @@ public interface PetRepository extends JpaRepository<Pet, Long> {
 
     List<Pet> getPetsByStatus(PetStatus status);
 
+    @Modifying
     @Query("UPDATE Pet p SET p.name = :name WHERE p.id = :id")
-    Pet updatePet(Long id, String name);
+    int updatePet(Long id, String name);
 
+    @Modifying
     @Query("UPDATE Pet p SET p.status = :status WHERE p.id = :id")
-    Pet updatePet(Long id, PetStatus status);
+    int updatePet(Long id, PetStatus status);
 
     // native query
+    // use a native query since INSERT is not a part of the JPA interface
+    @Modifying
     @Query(value = "INSERT INTO pet_photo_urls VALUE(:id, :url)", nativeQuery = true)
     void addPhotoUrl(Long id, String url);
 }
