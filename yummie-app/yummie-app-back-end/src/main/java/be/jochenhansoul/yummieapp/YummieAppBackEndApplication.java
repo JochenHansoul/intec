22b4590eb4e7c27313da9ff2937567997b4bd0e1
JochenHansoul <jochenhansoul101@hotmail.com>
2021-12-31 18:20:23 +0100

    Temporary disables security

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
@@ -2,8 +2,9 @@ package be.jochenhansoul.yummieapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class YummieAppBackEndApplication {
 
     public static void main(String[] args) {
