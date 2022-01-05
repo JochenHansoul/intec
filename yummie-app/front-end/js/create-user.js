423290f9d6ef5a4776e79f2abac2aabfd7cfd0fc
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-05 23:21:58 +0100

    Removed unnecessary if structure by using an element selector

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -2,15 +2,12 @@
 
 const getFormObject = form => {
     const o = {};
-    for (const element of form.elements) {
-        if (element.nodeName === "INPUT") {;
-            o[element.name] = element.value;
-        }
+    for (const element of form.getElementsByTagName("input")) {
+        o[element.name] = element.value;
     }
     return o;
 }
 
 const form = document.forms[0];
 const user = getFormObject(form);
-console.log(user.name);
-console.log(user.password);
+console.log(user);
