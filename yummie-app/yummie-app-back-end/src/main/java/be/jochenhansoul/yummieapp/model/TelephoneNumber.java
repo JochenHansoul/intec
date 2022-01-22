bc7568fc42714fc879bc6e42ca093a9311398302
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-23 00:10:21 +0100

    Changed properties and added getDescription method

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/TelephoneNumber.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/TelephoneNumber.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Optional;
 
 @Entity
 @Setter
@@ -17,7 +18,11 @@ public class TelephoneNumber {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idTelephoneNumber;
-    private String description;
     private long number;
-    private boolean isMainTelephone;
+    private boolean isValidated = false;
+    private String description = null;
+
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(this.description);
+    }
 }
