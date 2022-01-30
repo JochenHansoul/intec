a6e12975c6b524efb3076a23273d860a3469a8fb
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-30 02:30:25 +0100

    Created a simple user object and returned it

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
@@ -1,17 +1,32 @@
 package be.jochenhansoul.yummieapp.controller;
 
+import be.jochenhansoul.yummieapp.model.user.Gender;
+import be.jochenhansoul.yummieapp.model.user.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @PostMapping
-    public ResponseEntity addUser(@RequestParam("name") String name, @RequestParam("password") String password) {
-        return ResponseEntity.ok("user created: " + name + " " + password);
+    public ResponseEntity addUser(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("gender") Gender gender,
+            @RequestParam("password") String password) {
+        User user = new User();
+        user
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setGender(gender)
+                .setPassword(password)
+                .setCreationDate(LocalDate.now());
+        return ResponseEntity.ok(user.toString());
     }
 }
