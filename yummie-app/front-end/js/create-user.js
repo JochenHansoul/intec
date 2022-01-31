23e4b6f4d9136d2e774a405861110f522a63986a
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-31 04:37:35 +0100

    Created function to send an object to the server and added an event listener to the user form

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// constants
+const USER_FORM = document.forms.user;
+
+// functions
 const getFormObject = form => {
     const inputs = form.getElementsByTagName("input")
     return Array.prototype.slice.call(inputs).reduce(
@@ -9,7 +13,36 @@ const getFormObject = form => {
         },
         {}
     );
-}
+};
 
-const user = getFormObject(document.forms.user);
-console.log(user);
+const sendObject = (url, obj) => {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Accept": "applicatin/json",
+      "Access-Control-Allow-Origin": "*", // paybe problem with addblocker?
+      "Content-Type": "application/json"
+    }
+  })
+  .then((response) => {
+    if (response.status === 201) {
+      return response.json();
+    } else {
+      throw new Error("error with status " + response.status);
+    }
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((exception) => {
+    console.log(exception);
+  })
+};
+
+// main
+USER_FORM.addEventListener("submit", event => {
+  event.preventDefault();
+  const user = getFormObject(USER_FORM);
+  sendObject(USER_FORM.action, user);
+});
