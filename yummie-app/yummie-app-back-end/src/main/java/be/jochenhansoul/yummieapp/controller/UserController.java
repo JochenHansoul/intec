6df4b71e5bcca7f579f6be46962f216a4bf64a20
JochenHansoul <jochenhansoul101@hotmail.com>
2022-01-31 04:38:34 +0100

    Changed all request parameters to one request body for the User object

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
@@ -1,39 +1,16 @@
 package be.jochenhansoul.yummieapp.controller;
 
-import be.jochenhansoul.yummieapp.model.user.Email;
-import be.jochenhansoul.yummieapp.model.user.Gender;
 import be.jochenhansoul.yummieapp.model.user.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @PostMapping
-    public ResponseEntity addUser(
-            @RequestParam("firstName") String firstName,
-            @RequestParam("lastName") String lastName,
-            @RequestParam("gender") Gender gender,
-            @RequestParam("password") String password,
-            @RequestParam("emailAddress") String emailAddress,
-            @RequestParam(value = "emailDescription", required = false) String emailDescription) {
-        Email email = new Email()
-                .setEmailAddress(emailAddress)
-                .setDescription(emailDescription)
-                .setMainEmail(true);
-        User user = new User()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(gender)
-                .setPassword(password)
-                .setCreationDate(LocalDate.now())
-                .addEmail(email);
-        return ResponseEntity.ok(user);
+    public ResponseEntity addUser(@RequestBody User user) {
+        System.out.println(user);
+        return ResponseEntity.status(201).body(user);
     }
 }
