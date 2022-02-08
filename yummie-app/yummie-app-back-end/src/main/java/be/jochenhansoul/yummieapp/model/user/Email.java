77106872265170a6f09b7cc537260552235eab4a
JochenHansoul <jochenhansoul101@hotmail.com>
2022-02-08 04:15:51 +0100

    Added @ToString annotation

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/Email.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/Email.java
@@ -3,6 +3,7 @@ package be.jochenhansoul.yummieapp.model.user;
 import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.Optional;
 @Setter
 @Getter
 @Accessors(chain = true)
+@ToString
 public class Email {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
