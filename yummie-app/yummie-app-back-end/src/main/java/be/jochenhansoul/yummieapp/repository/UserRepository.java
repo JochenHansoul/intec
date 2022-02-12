4d18b6aacc14cf63136c8f8c8d64a56f6e867dbb
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-12 19:37:16 +0100

    Created the UserRepository interface

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package be.jochenhansoul.yummieapp.repository;
+
+import be.jochenhansoul.yummieapp.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
