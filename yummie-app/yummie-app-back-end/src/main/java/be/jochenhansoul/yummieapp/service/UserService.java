e80336c2d434d99e73cdbea08da1ff786abb5f5d
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-12 19:42:38 +0100

    Created method to save an user

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserService.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserService.java
@@ -1,7 +1,10 @@
 package be.jochenhansoul.yummieapp.service;
 
+import be.jochenhansoul.yummieapp.model.user.User;
 import be.jochenhansoul.yummieapp.repository.UserRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class UserService {
@@ -11,5 +14,9 @@ public class UserService {
     public UserService(UserRepository userRepository, UserValidator userValidator) {
         this.USER_REPOSITORY = userRepository;
         this.USER_VALIDATOR = userValidator;
+    }
+
+    public Optional<User> saveUser(User user) {
+        return Optional.of(USER_REPOSITORY.save(user));
     }
 }
