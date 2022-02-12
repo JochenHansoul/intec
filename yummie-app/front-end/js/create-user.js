572879d26964790d9431f7192840216953db9679
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-12 18:05:11 +0100

    Removed all the local functions and used the functions in the modules

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -1,51 +1,16 @@
 "use strict";
 
+// functions
+const sendResponse = x => { console.log(x); };
+const sendError = x => { console.log(x); };
+
 // constants
 const USER_FORM = document.forms.user;
-
-// functions
-const getFormObject = form => {
-    const inputs = form.getElementsByTagName("input")
-    return Array.prototype.slice.call(inputs).reduce(
-        (obj, tag) => {
-            obj[tag.name] = tag.value;
-            return obj;
-        },
-        {}
-    );
-};
-
-const sendObject = (url, obj, method = "GET") => {
-  fetch(url, {
-    method: method,
-    body: JSON.stringify(obj),
-    headers: {
-      "Accept": "applicatin/json",
-      "Content-Type": "application/json"
-    }
-  })
-  .then((response) => {
-    if (response.status === 201) {
-      console.log("correct status");
-      return response.json();
-    } else {
-      throw new Error("error with status " + response.status);
-    }
-  })
-  .then((result) => {
-    console.log("inside result block");
-    console.log(result);
-  })
-  .catch((exception) => {
-    console.log("inside exception block");
-    console.log(exception.message);
-  })
-};
 
 // main
 USER_FORM.addEventListener("submit", event => {
   event.preventDefault();
   const user = getFormObject(USER_FORM.user);
   user["emails"] = [getFormObject(USER_FORM.email)];
-  sendObject(USER_FORM.action, user, USER_FORM.method);
+  sendPost(USER_FORM.action, user, sendResponse, sendError);
 });
