ef37b1c824862df32ad7fef21c5010046cc6e0bb
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-13 05:27:14 +0100

    Managed to store an User in the database and return the users email and password on success.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/UserController.java
@@ -1,24 +1,40 @@
 package be.jochenhansoul.yummieapp.controller;
 
 import be.jochenhansoul.yummieapp.model.user.User;
+import be.jochenhansoul.yummieapp.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
+    private final UserService USER_SERVICE;
+
+    public UserController(UserService userService) {
+        this.USER_SERVICE = userService;
+    }
+
     @CrossOrigin
     @PostMapping
     public ResponseEntity<String> addUser(@RequestBody User user) throws JsonProcessingException {
-        System.out.println("successfully created user");
-        //System.out.println(user);
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(user);
-        //System.out.println("ResultingJSONstring = " + json);
-        return ResponseEntity.status(201).body(json); // "{\"json\":\"user\"}"
+        //System.out.println("successfully created user " + user);
+        Optional<User> optionalUser = this.USER_SERVICE.saveUser(user);
+        //System.out.println("after user service");
+        if (optionalUser.isPresent()) {
+            User savedUser = optionalUser.get();
+            //System.out.println(savedUser);
+            /*ObjectMapper mapper = new ObjectMapper();
+            System.out.println("turning into a json string:");
+            String json = mapper.writeValueAsString(savedUser);
+            System.out.println("ResultingJSONstring = " + json);*/
+            return ResponseEntity.status(201).body("{\"email\":\"" + savedUser.getEmail() + "\",\"password\":\"" + user.getPassword() + "\"}"); // "{\"key\":\"value\"}"
+        } else {
+            return ResponseEntity.status(422).body("user not created");
+        }
     }
 
     @ExceptionHandler(Exception.class)
