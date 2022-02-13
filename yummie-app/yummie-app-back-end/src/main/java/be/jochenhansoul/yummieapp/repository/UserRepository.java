9abfd76a067d376f2fdee5ce1eb2971ea39093b2
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-13 05:27:43 +0100

    Added method to get an user based on their email

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/UserRepository.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/UserRepository.java
@@ -4,4 +4,5 @@ import be.jochenhansoul.yummieapp.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User getUsersByEmail(String email);
 }
