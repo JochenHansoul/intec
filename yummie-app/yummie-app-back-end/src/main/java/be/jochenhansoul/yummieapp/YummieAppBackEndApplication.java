73a8fb6fe755d1556d8fbbdcce5b931ec5198a96
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-13 05:24:34 +0100

    Added code (in comments) in case I ever need to create an user by default.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
@@ -1,14 +1,29 @@
 package be.jochenhansoul.yummieapp;
 
+import be.jochenhansoul.yummieapp.model.user.User;
+import be.jochenhansoul.yummieapp.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class YummieAppBackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YummieAppBackEndApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(YummieAppBackEndApplication.class, args);
+        /*UserRepository userRepository = ctx.getBean(UserRepository.class);
+
+        User user = new User();
+        user.setFirstName("first1");
+        user.setLastName("last1");
+        user.setPassword("pass1");
+        user.setEmail("first.last@outlook.be");
+        user.setTelephone("+32 111 11 11 11");
+        userRepository.save(user);
+
+        User user2 = userRepository.getUsersByEmail("first.last@outlook.be");
+        System.out.println(user2);*/
     }
 
 }
