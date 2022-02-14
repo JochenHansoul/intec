a3c84359e165988593b54ad7d38d3a54eb40535d
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-14 21:04:33 +0100

    Changed the method to verify if the two given passwords are the same and if not send an error message to the password output element. Also implemented code.

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -7,9 +7,16 @@ const sendError = x => { console.log(x); };
 // constants
 const USER_FORM = document.forms.user;
 
+
 // main
 USER_FORM.addEventListener("submit", event => {
   event.preventDefault();
-  const user = getFormObject(USER_FORM);
-  sendPost(USER_FORM.action, user, sendResponse, sendError);
+  const INPUTS = USER_FORM.querySelectorAll("input[type=\"password\"]");
+  if (INPUTS[0].value === INPUTS[1].value) {
+    const user = getFormObject(USER_FORM);
+    sendPost(USER_FORM.action, user, sendResponse, sendError);
+  } else {
+    USER_FORM.querySelector("output[name=\"password\"]")
+      .innerText = "passwords are not the same";
+  }
 });
