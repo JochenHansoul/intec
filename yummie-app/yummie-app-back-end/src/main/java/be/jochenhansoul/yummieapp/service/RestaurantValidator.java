bbb4c7abace5f9579cf0f33571ff656b3b142906
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 21:42:08 +0100

    Created start of RestaurantValidator.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantValidator.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantValidator.java
@@ -1,7 +1,14 @@
 package be.jochenhansoul.yummieapp.service;
 
+import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestaurantValidator {
+
+    public boolean validateRestaurant(Restaurant restaurant) {
+        return restaurant.getName().length() > 1
+                && restaurant.getLocation() != null;
+        // todo: location validator
+    }
 }
