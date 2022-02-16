b4014baae137f61d63df72502c4fe1abb28082a6
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 15:21:33 +0100

    Removed the RestaurantUser and RestaurantUserRepository clas and Interface.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/RestaurantUserRepository.java
+++
@@ -1,8 +0,0 @@
-package be.jochenhansoul.yummieapp.repository;
-
-import be.jochenhansoul.yummieapp.model.user.RestaurantUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RestaurantUserRepository extends JpaRepository<RestaurantUser, Long> {
-    RestaurantUser getRestaurantUserByEmail(String email);
-}
