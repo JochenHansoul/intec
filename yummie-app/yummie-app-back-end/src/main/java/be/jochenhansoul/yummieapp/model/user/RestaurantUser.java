29b9e5c8d9ec9510a5cd1bb356393d2cbd28a89e
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 01:28:35 +0100

    Added constructors and method to add a restaurant to a RestaurantUser.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/RestaurantUser.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/RestaurantUser.java
@@ -1,2 +1,45 @@
-package be.jochenhansoul.yummieapp.model.user;public class RestaurantUser {
+package be.jochenhansoul.yummieapp.model.user;
+
+import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance
+@NoArgsConstructor
+@Setter
+@Getter
+@Accessors(chain = true)
+public class RestaurantUser extends User {
+    @OneToMany
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantUser(
+            User user,
+            List<Restaurant> restaurants,
+            String restaurantDocumentLocation,
+            boolean isValidRestaurantUser) {
+
+        super(user);
+        this.restaurants = restaurants;
+    }
+
+    public RestaurantUser(User user, Restaurant restaurant) {
+        super(user);
+        this.restaurants.add(restaurant);
+    }
+
+    public void addRestaurant(Restaurant restaurant) {
+        this.restaurants.add(restaurant);
+    }
 }
