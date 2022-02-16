5b3715a6e676e8f0d6e26bccfbcd1bfbbe11826d
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-17 00:10:20 +0100

    Put all the selectors out of the eventListener and send the response of the server to the document (both positive as negative).

--- yummie-app/front-end/js/create-restaurant.js
+++ yummie-app/front-end/js/create-restaurant.js
@@ -1,32 +1,34 @@
 "use strict";
 
-// functions
-const sendResponse = x => { console.log(x); };
-const sendError = x => { console.log(x); };
+// constants
+const RESTAURANT_FORM = document.forms.restaurant;
+const INPUTS = RESTAURANT_FORM.getElementsByTagName("input");
+const OUTPUTS = RESTAURANT_FORM.querySelectorAll("output");
+const LATTITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(2) label input");
+const LONGITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(3) label input");
 
+// functions
 const calculateCoordinate = (degrees, minutes, seconds) => {
   const N = parseInt(degrees);
   const RESULT = Math.abs(N) + (parseInt(minutes) / 60) + (parseInt(seconds) / 60);
   return (N < 0) ? RESULT * -1 : RESULT;
 };
 
+const sendResponse = x => OUTPUTS[2].innerText = "Restaurant succesfully saved";
 
-// constants
-const RESTAURANT_FORM = document.forms.restaurant;
+const sendError = x => OUTPUTS[2].innerText = "Could not save restaurant. Error: " + x.message;
 
 
 // main
 RESTAURANT_FORM.addEventListener("submit", event => {
   event.preventDefault();
-  // location verification
-  const LATTITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(2) label input");
-  const LONGITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(3) label input");
 
+  // clearing output fields
+  OUTPUTS.forEach(output => output.innerText = "");
+
+  // location verification
   const LATTITUDE = calculateCoordinate(LATTITUDES[0].value, LATTITUDES[1].value, LATTITUDES[2].value);
   const LONGITUDE = calculateCoordinate(LONGITUDES[0].value, LONGITUDES[1].value, LONGITUDES[2].value);
-
-  const OUTPUTS = RESTAURANT_FORM.querySelectorAll("output");
-  OUTPUTS.forEach(output => output.innerText = ""); // clearing output fields
   if (LATTITUDE < -90 || LATTITUDE > 90) {
     OUTPUTS[0].innerText = "lattitude must be between -90 and 90";
     if (LONGITUDE < -180 || LONGITUDE > 180) {
@@ -36,7 +38,6 @@ RESTAURANT_FORM.addEventListener("submit", event => {
       OUTPUTS[1].innerText = "longitude must be between -180 and 180";
   } else {
     // creating and sending form object
-    const INPUTS = RESTAURANT_FORM.getElementsByTagName("input");
     const RESTAURANT = {
       idUser: 1,
       name: INPUTS.restaurantName.value,
