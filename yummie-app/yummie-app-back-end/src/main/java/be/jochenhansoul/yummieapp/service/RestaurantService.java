637915374315527f9b1981931134cceb3d884841
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 21:52:19 +0100

    Creatd a method that saved the restaurant to the database.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantService.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantService.java
@@ -1,7 +1,25 @@
 package be.jochenhansoul.yummieapp.service;
 
+import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
+import be.jochenhansoul.yummieapp.repository.RestaurantRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class RestaurantService {
+
+    private final RestaurantValidator RESTAURANT_VALIDATOR;
+    private final RestaurantRepository RESTAURANT_REPOSITORY;
+
+    public RestaurantService(RestaurantValidator RESTAURANT_VALIDATOR, RestaurantRepository RESTAURANT_REPOSITORY) {
+        this.RESTAURANT_VALIDATOR = RESTAURANT_VALIDATOR;
+        this.RESTAURANT_REPOSITORY = RESTAURANT_REPOSITORY;
+    }
+
+    public Optional<Restaurant> saveRestaurantAndAddToUser(Restaurant restaurant) {
+        return (this.RESTAURANT_VALIDATOR.validateRestaurant(restaurant))
+                ? Optional.of(this.RESTAURANT_REPOSITORY.save(restaurant))
+                : Optional.empty();
+    }
 }
