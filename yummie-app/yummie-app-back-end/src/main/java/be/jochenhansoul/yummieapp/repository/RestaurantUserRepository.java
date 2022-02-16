2d1dcc7548ddb0560081b2264115932e6703b26f
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 02:07:34 +0100

    CreatedRestaurantUserRepository.

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/RestaurantUserRepository.java
@@ -0,0 +1,7 @@
+package be.jochenhansoul.yummieapp.repository;
+
+import be.jochenhansoul.yummieapp.model.user.RestaurantUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantUserRepository extends JpaRepository<RestaurantUser, Long> {
+}
