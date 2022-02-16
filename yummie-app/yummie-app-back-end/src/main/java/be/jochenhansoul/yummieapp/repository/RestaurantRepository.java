d965fc4d4bde5420afa9ae6d1a0155ec38cb857b
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 04:09:58 +0100

    Added method to get a restaurant based on it's id and one to get a list of restaurants based on the id of the restaurant user.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/RestaurantRepository.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/RestaurantRepository.java
@@ -2,6 +2,14 @@ package be.jochenhansoul.yummieapp.repository;
 
 import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    @Query("SELECT r FROM Restaurant r WHERE r.idRestaurant = :id")
+    Restaurant getRestaurantByIdRestaurant(Long id);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.idRestaurantUser = :id")
+    List<Restaurant> getRestaurantsByIdRestaurantUser(Long id);
 }
