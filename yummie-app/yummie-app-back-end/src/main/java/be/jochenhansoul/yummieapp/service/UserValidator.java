ba6407b4825de359a075f3fd240f1113316fac0a
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 22:10:24 +0100

    Added methods to validate an user and an email address.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserValidator.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserValidator.java
@@ -1,7 +1,26 @@
 package be.jochenhansoul.yummieapp.service;
 
+import be.jochenhansoul.yummieapp.model.user.User;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class UserValidator {
+
+    public boolean validateUser(User user) {
+        return user.getFirstName().length() > 5
+                && user.getLastName().length() > 5
+                && user.getPassword().length() > 5
+                && validateEmail(user.getEmail())
+                && user.getGender() != null
+                && user.getBirthDate() != null
+                && user.getBirthDate().isAfter(LocalDate.of(1940, 0, 0))
+                && user.getBirthDate().isBefore(LocalDate.now());
+    }
+
+    public boolean validateEmail(String email) {
+        String[] array = email.split("@", -1);
+        return array.length == 2;
+    }
 }
