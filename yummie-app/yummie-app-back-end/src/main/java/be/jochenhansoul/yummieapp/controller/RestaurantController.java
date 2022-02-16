c85916ddf8b4a52e89e258f1bc7daae09a40387c
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 23:29:59 +0100

    Saved the location of the restaurant to the database and added it to the restaurant.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/RestaurantController.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/controller/RestaurantController.java
@@ -1,13 +1,12 @@
 package be.jochenhansoul.yummieapp.controller;
 
+import be.jochenhansoul.yummieapp.model.general.Location;
 import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
+import be.jochenhansoul.yummieapp.service.LocationService;
 import be.jochenhansoul.yummieapp.service.RestaurantService;
 import be.jochenhansoul.yummieapp.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -15,19 +14,29 @@ import java.util.Optional;
 @RequestMapping("restaurant")
 public class RestaurantController {
 
+    private final LocationService LOCATION_SERVICE;
     private final RestaurantService RESTAURANT_SERVICE;
     private final UserService USER_SERVICE;
 
-    public RestaurantController(RestaurantService restaurant_service, UserService user_service) {
+    public RestaurantController(LocationService locationService, RestaurantService restaurant_service, UserService user_service) {
+        this.LOCATION_SERVICE = locationService;
         this.RESTAURANT_SERVICE = restaurant_service;
         this.USER_SERVICE = user_service;
     }
 
+    @CrossOrigin
     @PostMapping("/create")
     public ResponseEntity<String> addRestaurant(@RequestBody Restaurant restaurant) {
-        Optional<Restaurant> optionalRestaurant = this.RESTAURANT_SERVICE.saveRestaurantAndAddToUser(restaurant);
-        return optionalRestaurant.map(value -> (this.USER_SERVICE.addRestaurantToUser(value).isPresent())
-                ? ResponseEntity.status(201).body("") : ResponseEntity.status(400).body("Could not add restaurant to user."))
-                .orElseGet(() -> ResponseEntity.status(400).body("Could not add restaurant to database."));
+        Optional<Location> optionalLocation = this.LOCATION_SERVICE.saveLocation(restaurant.getLocation());
+        if (optionalLocation.isPresent()) {
+            restaurant.setLocation(optionalLocation.get());
+            Optional<Restaurant> optionalRestaurant = this.RESTAURANT_SERVICE.saveRestaurantAndAddToUser(restaurant);
+            return optionalRestaurant.map(value -> (this.USER_SERVICE.addRestaurantToUser(value).isPresent())
+                            ? ResponseEntity.status(201).body("{\"idRestaurant\":\"" + optionalRestaurant.get().getIdRestaurant() + "\"}")
+                            : ResponseEntity.status(400).body("Could not add restaurant to user."))
+                    .orElseGet(() -> ResponseEntity.status(400).body("Could not add restaurant to database."));
+        } else {
+            return ResponseEntity.status(400).body("Could not add location to database.");
+        }
     }
 }
