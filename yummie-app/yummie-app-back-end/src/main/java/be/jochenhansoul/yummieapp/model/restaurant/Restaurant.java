70d46a0c7b511a09b4ca45665265573a00b0c31b
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 01:29:37 +0100

    Added restaurantCertificateLocation and restaurantCertificateIsValid properties, changed the long for the id into Long and refactored code.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/restaurant/Restaurant.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/restaurant/Restaurant.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,28 +21,39 @@ import java.util.List;
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idRestaurant;
+    private Long idRestaurant;
+
     private String name;
-    @OneToOne
-    private RestaurantAddress address;
-    @ElementCollection
-    private List<String> telephoneNumbers;
-    @ElementCollection
-    private List<String> emailAddresses;
-    @ManyToMany
-    private List<FoodCategory> categories;
-    private String restaurantDescription;
-    @OneToMany
-    private List<OpenHour> openHours;
-    private String openHoursDescription;
-    @OneToMany
-    private List<Dish> dishes;
+    /*@OneToOne
+    private RestaurantAddress address;*/
     @OneToOne
     @NotNull
     private Location location;
+    @NotNull
+    private LocalDate creationDate = LocalDate.now();
+
+    @NotNull
+    private String restaurantCertificateLocation;
+    @NotNull
+    private boolean certificateIsValid = false;
+    @NotNull
+    private boolean isVisible = false;
+
+    @ElementCollection
+    private List<String> telephoneNumbers = new ArrayList<>();
+    @ElementCollection
+    private List<String> emailAddresses = new ArrayList<>();
+    @ManyToMany
+    private List<FoodCategory> categories = new ArrayList<>();
     @OneToMany
-    private List<Picture> pictures;
+    private List<OpenHour> openHours = new ArrayList<>();
     @OneToMany
-    private List<Review> reviews;
-    private boolean isVisible;
+    private List<Dish> dishes = new ArrayList<>();
+    @OneToMany
+    private List<Picture> pictures = new ArrayList<>();
+    @OneToMany
+    private List<Review> reviews = new ArrayList<>();
+
+    private String openHoursDescription = null;
+    private String restaurantDescription = null;
 }
