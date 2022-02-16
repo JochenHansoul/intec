9a3f07b801212688ad304c0f41d41c7b852448ac
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-16 20:07:28 +0100

    Verifyed coordinates input and send error message. Restaurant object is now send to server.

--- yummie-app/front-end/js/create-restaurant.js
+++ yummie-app/front-end/js/create-restaurant.js
@@ -4,23 +4,12 @@
 const sendResponse = x => { console.log(x); };
 const sendError = x => { console.log(x); };
 
-const calculateCoordinate = (degrees, minutes, seconds, range) => {
+const calculateCoordinate = (degrees, minutes, seconds) => {
   const N = parseInt(degrees);
   const RESULT = Math.abs(N) + (parseInt(minutes) / 60) + (parseInt(seconds) / 60);
-  if (RESULT > range) {
-    throw new Exception("must be between -" + range + "and "+ range);
-  } else {
-    return (N < 0) ? RESULT * -1 : RESULT;
-  }
+  return (N < 0) ? RESULT * -1 : RESULT;
 };
 
-const calculateLattitude = (degrees, minutes, seconds) => {
-  return calculateCoordinate(degrees, minutes, seconds, 90);
-};
-
-const calculateLongitude = (degrees, minutes, seconds) => {
-  return calculateCoordinate(degrees, minutes, seconds, 180);
-};
 
 // constants
 const RESTAURANT_FORM = document.forms.restaurant;
@@ -29,28 +18,30 @@ const RESTAURANT_FORM = document.forms.restaurant;
 // main
 RESTAURANT_FORM.addEventListener("submit", event => {
   event.preventDefault();
-  // location
+  // location verification
   const LATTITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(2) label input");
   const LONGITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(3) label input");
-  const LOCATION = {
-    lattitude: calculateLattitude(LATTITUDES[0].value, LATTITUDES[1].value, LATTITUDES[2].value),
-    longitude: calculateLongitude(LONGITUDES[0].value, LONGITUDES[1].value, LONGITUDES[2].value)
-  };
 
-  // restaurant
-  const INPUTS = RESTAURANT_FORM.getElementsByTagName("input")
-  const RESTAURANT = {
-    name: INPUTS.restaurantName.value,
-    location: LOCATION,
-    file: INPUTS.certificate.value
-  };
-  console.log(RESTAURANT);
+  const LATTITUDE = calculateCoordinate(LATTITUDES[0].value, LATTITUDES[1].value, LATTITUDES[2].value);
+  const LONGITUDE = calculateCoordinate(LONGITUDES[0].value, LONGITUDES[1].value, LONGITUDES[2].value);
 
-  /*if (INPUTS[0].value === INPUTS[1].value) {
-    const user = getFormObject(USER_FORM);
-    sendPost(USER_FORM.action, user, sendResponse, sendError);
+  const OUTPUTS = RESTAURANT_FORM.querySelectorAll("output");
+  if (LATTITUDE < -90 || LATTITUDE > 90) {
+      OUTPUTS[0].innerText = "lattitude must be between -90 and 90";
+  } else if (LONGITUDE < -180 || LONGITUDE > 180) {
+      OUTPUTS[1].innerText = "longitude must be between -180 and 180";
   } else {
-    USER_FORM.querySelector("output[name=\"password\"]")
-      .innerText = "passwords are not the same";
-  }*/
+    // creating and sending form object
+    const INPUTS = RESTAURANT_FORM.getElementsByTagName("input");
+    const RESTAURANT = {
+      idUser: 1,
+      name: INPUTS.restaurantName.value,
+      location: {
+        lattitude: LATTITUDE,
+        longitude: LONGITUDE
+      },
+      file: INPUTS.certificate.value
+    };
+    sendPost(RESTAURANT_FORM.action, RESTAURANT, sendResponse, sendError);
+  }
 });
