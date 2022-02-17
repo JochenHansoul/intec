e3a0c24ed6f2361d4c0bcdadc00ae6c0021a97de
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-17 14:58:47 +0100

    Reformatted code. Created functions to correctly transfer the server responses.

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -1,22 +1,29 @@
 "use strict";
 
-// functions
-const sendResponse = x => { console.log(x); };
-const sendError = x => { console.log(x); };
-
 // constants
 const USER_FORM = document.forms.user;
+const PASSWORD_INPUTS = USER_FORM.querySelectorAll("input[type=\"password\"]");
+const PASSWORD_OUTPUT = USER_FORM.querySelector("output[name=\"password\"]");
+const FORM_OUTPUT = USER_FORM.querySelector("output[name=\"response\"]");
 
+// functions
+const sendResponse = user => {
+  localStorage.setItem("userId", user.idUser);
+  localStorage.setItem("nameUser", user.nameUser);
+  location.href = "file:///home/jochenhansoul/Projects/intec/yummie-app/front-end/find-restaurants.html";
+};
+
+const sendError = error => {
+  FORM_OUTPUT.innerText = "Error creating user: " + error.message;
+};
 
 // main
 USER_FORM.addEventListener("submit", event => {
   event.preventDefault();
-  const INPUTS = USER_FORM.querySelectorAll("input[type=\"password\"]");
-  if (INPUTS[0].value === INPUTS[1].value) {
-    const user = getFormObject(USER_FORM);
-    sendPost(USER_FORM.action, user, sendResponse, sendError);
+  if (PASSWORD_INPUTS[0].value === PASSWORD_INPUTS[1].value) {
+    const USER = getFormObject(USER_FORM);
+    sendPost(USER_FORM.action, USER, sendResponse, sendError);
   } else {
-    USER_FORM.querySelector("output[name=\"password\"]")
-      .innerText = "passwords are not the same";
+    PASSWORD_OUTPUT.innerText = "passwords are not the same";
   }
 });
