5976523dee8f1af08d3a710eb89349b4fa1202c1
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-17 02:01:54 +0100

    Changed minimumlength of name and password length and fixed isAfter big.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserValidator.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserValidator.java
@@ -9,13 +9,13 @@ import java.time.LocalDate;
 public class UserValidator {
 
     public boolean validateUser(User user) {
-        return user.getFirstName().length() > 5
-                && user.getLastName().length() > 5
-                && user.getPassword().length() > 5
+        return user.getFirstName().length() > 2
+                && user.getLastName().length() > 2
+                && user.getPassword().length() > 2
                 && validateEmail(user.getEmail())
                 && user.getGender() != null
                 && user.getBirthDate() != null
-                && user.getBirthDate().isAfter(LocalDate.of(1940, 0, 0))
+                && user.getBirthDate().isAfter(LocalDate.parse("1930-03-03"))
                 && user.getBirthDate().isBefore(LocalDate.now());
     }
 
