069edc4ab4d9758d47b2fc7731639deb3e42817c
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-17 23:06:35 +0100

    Added a method to get a list to get restaurants based on the given location (not sure if works).

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantService.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/RestaurantService.java
@@ -1,10 +1,11 @@
 package be.jochenhansoul.yummieapp.service;
 
+import be.jochenhansoul.yummieapp.model.general.Location;
 import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
 import be.jochenhansoul.yummieapp.repository.RestaurantRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.*;
 
 @Component
 public class RestaurantService {
@@ -21,5 +22,18 @@ public class RestaurantService {
         return (this.RESTAURANT_VALIDATOR.validateRestaurant(restaurant))
                 ? Optional.of(this.RESTAURANT_REPOSITORY.save(restaurant))
                 : Optional.empty();
+    }
+
+    public List<Restaurant> getRestaurantClosestTo(Location location) {
+        List<Restaurant> restaurantList = this.RESTAURANT_REPOSITORY.findAll();
+        /*Map<Restaurant, Double> restaurantMap = new HashMap<>();
+        for (Restaurant restaurant : restaurantList) {
+            restaurantMap.put(restaurant, location.getDistance(restaurant.getLocation()));
+        }
+        List<Map.Entry<Restaurant, Double>> list = new ArrayList<>(restaurantMap.entrySet());
+        list.sort(Map.Entry.comparingByValue());*/
+
+        restaurantList.sort((restaurant, t1) -> (int) restaurant.getLocation().getDistance(location));
+        return restaurantList;
     }
 }
