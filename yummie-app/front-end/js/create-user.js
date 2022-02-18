bdab5b305868c99204a2242f44a3bfb62c60cab0
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-18 07:23:55 +0100

    Changed localStorage to sessionStorage and camelcase to kebobcase.

--- yummie-app/front-end/js/create-user.js
+++ yummie-app/front-end/js/create-user.js
@@ -8,8 +8,8 @@ const FORM_OUTPUT = USER_FORM.querySelector("output[name=\"response\"]");
 
 // functions
 const sendResponse = user => {
-  localStorage.setItem("userId", user.idUser);
-  localStorage.setItem("nameUser", user.nameUser);
+  sessionStorage.setItem("user-id", user.idUser);
+  sessionStorage.setItem("name-user", user.nameUser);
   location.href = "file:///home/jochenhansoul/Projects/intec/yummie-app/front-end/find-restaurants.html";
 };
 
--- yummie-app/front-end/js/modules/get-user-name.js
+++ yummie-app/front-end/js/modules/get-user-name.js
@@ -2,13 +2,12 @@
 
 const USER_NAME_INPUT = document.getElementById("user-name");
 
-const USER_NAME_LOCAL_STORAGE = window.localStorage.getItem("nameUser");
+const USER_NAME_LOCAL_STORAGE = window.sessionStorage.getItem("name-user");
 console.log("nameUser:");
-console.log(window.localStorage.getItem("nameUser"));
+console.log(window.sessionStorage.getItem("name-user"));
 console.log(USER_NAME_LOCAL_STORAGE);
 
 if (USER_NAME_LOCAL_STORAGE !== null) {
-  //USER_NAME_INPUT.innerText = USER_NAME_LOCAL_STORAGE;
   USER_NAME_INPUT.append(USER_NAME_LOCAL_STORAGE);
 }
 
