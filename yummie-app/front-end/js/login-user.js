2d41d2fccd850a792ba33ee8d53ba0c9c33690a6
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-18 07:33:42 +0100

    Created javascript file to log in an user.

---
+++ yummie-app/front-end/js/login-user.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// constants
+const USER_FORM = document.forms.user;
+const FORM_OUTPUT = USER_FORM.querySelector("output[name=\"response\"]");
+
+// functions
+const sendResponse = user => {
+  sessionStorage.setItem("user-id", user.idUser);
+  sessionStorage.setItem("name-user", user.nameUser);
+  location.href = "file:///home/jochenhansoul/Projects/intec/yummie-app/front-end/find-restaurants.html";
+};
+
+const sendError = error => {
+  FORM_OUTPUT.innerText = "Error creating user: " + error.message;
+};
+
+// main
+USER_FORM.addEventListener("submit", event => {
+  event.preventDefault();
+  const O = getFormObject(USER_FORM);
+  sendPost(USER_FORM.action, O, sendResponse, sendError);
+});
