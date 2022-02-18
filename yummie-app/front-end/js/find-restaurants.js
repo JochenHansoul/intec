52260e6c4d6f88a5449ad2ae9deef65b1c0f8807
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-18 08:23:02 +0100

    Added hidden css selector and made restaurant table appear when data is loaded.

--- yummie-app/front-end/js/find-restaurants.js
+++ yummie-app/front-end/js/find-restaurants.js
@@ -5,6 +5,7 @@ const RESTAURANT_FORM = document.forms.restaurant;
 const LATTITUDES = RESTAURANT_FORM.querySelectorAll(".lattitude input");
 const LONGITUDES = RESTAURANT_FORM.querySelectorAll(".longitude input");
 const OUTPUT = RESTAURANT_FORM.querySelector("output");
+const TABLE = document.getElementsByClassName("restaurant-table")[0];
 const RESTAURANT_TABLE = document.getElementById("restaurant-tbody");
 
 // functions
@@ -25,6 +26,7 @@ const createTable = tableData => {
 };
 
 const sendResponse = restaurants => {
+  TABLE.classList.remove("hidden");
   for (const RESTAURANT of restaurants) {
     const RESTAURANT_DATA = [RESTAURANT.name, RESTAURANT.location.latitude, RESTAURANT.location.longitude]
     RESTAURANT_TABLE.append(createTable(RESTAURANT_DATA));
