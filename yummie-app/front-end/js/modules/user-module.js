479b08470f9b5627b767405f6aa018ca3408540f
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-18 08:08:59 +0100

    Removed console.log().

--- yummie-app/front-end/js/modules/user-module.js
+++ yummie-app/front-end/js/modules/user-module.js
@@ -3,9 +3,6 @@
 const USER_NAME_INPUT = document.getElementById("user-name");
 
 const USER_NAME_LOCAL_STORAGE = window.sessionStorage.getItem("name-user");
-console.log("nameUser:");
-console.log(window.sessionStorage.getItem("name-user"));
-console.log(USER_NAME_LOCAL_STORAGE);
 
 if (USER_NAME_LOCAL_STORAGE !== null) {
   USER_NAME_INPUT.append(USER_NAME_LOCAL_STORAGE);
