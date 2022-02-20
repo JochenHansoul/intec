912aa231c9bcd7a3fecd20e1ffb033258299dff8
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-20 23:08:55 +0100

    Made the getUserByEmail return an optional User.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/UserRepository.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/repository/UserRepository.java
@@ -3,7 +3,9 @@ package be.jochenhansoul.yummieapp.repository;
 import be.jochenhansoul.yummieapp.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUsersByIdUser(Long id);
-    User getUsersByEmail(String email);
+    Optional<User> getUsersByEmail(String email);
 }
