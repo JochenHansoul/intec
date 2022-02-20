98205bbeb6cf1c817bd44bd890e6fc2bcba64bea
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-20 19:18:52 +0100

    Added two functions loginUser and logoutUser.

--- yummie-app/front-end/js/modules/user-module.js
+++ yummie-app/front-end/js/modules/user-module.js
@@ -6,9 +6,20 @@ const LOGOUT_ELEMENT = document.getElementById("js-logout-user");
 
 const LOGIN_DROPDOWN_LIST = document.querySelectorAll(".dropdown-list")[0];
 const LOGOUT_DROPDOWN_LIST = document.querySelectorAll(".dropdown-list")[1];
-
 // session storage
 const ID_SESSION_STORAGE = sessionStorage.getItem("id-user");
+
+
+// functions
+const loginUser = (id, fullName) => {
+  sessionStorage.setItem("id-user", id);
+  sessionStorage.setItem("full-name-user", fullName);
+};
+
+const logoutUser = () => {
+  sessionStorage.removeItem("id-user");
+  sessionStorage.removeItem("full-name-user");
+};
 
 
 // main
@@ -20,8 +31,7 @@ if (ID_SESSION_STORAGE !== null) {
   LOGOUT_DROPDOWN_LIST.classList.remove("hidden");
   // added logout click event
   LOGOUT_ELEMENT.addEventListener("click", event => {
-    sessionStorage.removeItem("full-name-user");
-    sessionStorage.removeItem("id-user");
+    logoutUser();
     location.href = "file:///home/jochenhansoul/Projects/intec/yummie-app/front-end/index.html";
   });
 }
