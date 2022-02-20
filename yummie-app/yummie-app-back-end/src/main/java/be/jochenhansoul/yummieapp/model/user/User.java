13251137874fc592266a0f04bebf103e0d0bab24
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-20 22:17:16 +0100

    Implemented UserDetails interface and added @EnumType string to UserType enum.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/User.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/User.java
@@ -5,9 +5,14 @@ import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
 import com.sun.istack.NotNull;
 import lombok.*;
 import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,10 +20,11 @@ import java.util.Set;
 @Setter
 @Getter
 @Accessors(chain = true)
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUser;
@@ -41,11 +47,14 @@ public class User {
     private String password;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     private UserType userType = UserType.USER;
     @NotNull
     private LocalDate creationDate = LocalDate.now();
     @NotNull
-    private boolean isValidated = false;
+    private boolean locked = false;
+    @NotNull
+    private boolean enabled = false;
 
     @OneToOne
     private Location defaultSearchLocation = null;
@@ -56,6 +65,38 @@ public class User {
     private Set<Restaurant> ownedRestaurants = new HashSet<>();
 
     // methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(userType.name());
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getUsername() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+
     public void addFavoriteRestaurant(Restaurant restaurant) {
         this.favoriteRestaurants.add(restaurant);
     }
