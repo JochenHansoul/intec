1af3ff2922c6574f761b614e9836405d0f5cdab1
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 02:27:01 +0100

    Added signUpUser method

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserService.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/UserService.java
@@ -1,23 +1,42 @@
 package be.jochenhansoul.yummieapp.service;
 
 import be.jochenhansoul.yummieapp.model.restaurant.Restaurant;
+import be.jochenhansoul.yummieapp.model.user.ConfirmationToken;
 import be.jochenhansoul.yummieapp.model.user.User;
 import be.jochenhansoul.yummieapp.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
+    private final BCryptPasswordEncoder BCRYPT_PASSWORD_ENCODER = new BCryptPasswordEncoder();
+
     private final UserRepository USER_REPOSITORY;
     private final UserValidator USER_VALIDATOR;
+    private final ConfirmationTokenService CONFIRMATION_TOKEN_SERVICE;
 
-    public UserService(UserRepository userRepository, UserValidator userValidator) {
+    public UserService(UserRepository userRepository, UserValidator userValidator, ConfirmationTokenService confirmationTokenService) {
         this.USER_REPOSITORY = userRepository;
         this.USER_VALIDATOR = userValidator;
+        this.CONFIRMATION_TOKEN_SERVICE = confirmationTokenService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return this.USER_REPOSITORY.getUsersByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+    }
+
+    void signUpUser(User user) {
+        user.setPassword(this.BCRYPT_PASSWORD_ENCODER.encode(user.getPassword()));
+        final User createdUser = this.USER_REPOSITORY.save(user);
+        final ConfirmationToken confirmationToken = new ConfirmationToken(user);
+        this.CONFIRMATION_TOKEN_SERVICE.saveConfirmationToken(confirmationToken);
     }
 
     public Optional<User> saveUser(User user) {
@@ -41,11 +60,5 @@ public class UserService implements UserDetailsService {
         } else {
             return Optional.empty();
         }
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return this.USER_REPOSITORY.getUsersByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
     }
 }
