bdf651fd449f2880262bc2df77ef81b6141075d7
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 11:18:15 +0100

    Uncommented security.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/YummieAppBackEndApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class YummieAppBackEndApplication {
 
     public static void main(String[] args) {
