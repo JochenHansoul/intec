c5aa1ce2832612523381d55aab55e7e96630bec4
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 02:26:32 +0100

    Made constructor public

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/ConfirmationToken.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/model/user/ConfirmationToken.java
@@ -22,16 +22,13 @@ public class ConfirmationToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String confirmationToken;
-
     private LocalDate createdDate;
-
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
-    ConfirmationToken(User user) {
+    public ConfirmationToken(User user) {
         this.user = user;
         this.createdDate = LocalDate.now();
         this.confirmationToken = UUID.randomUUID().toString();
