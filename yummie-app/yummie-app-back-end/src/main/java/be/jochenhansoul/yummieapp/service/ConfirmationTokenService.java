a196dad1f0470a985b825ef8c92f908a4d18e463
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 02:13:29 +0100

    Created confirmationtoken service.

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/service/ConfirmationTokenService.java
@@ -0,0 +1,20 @@
+package be.jochenhansoul.yummieapp.service;
+
+import be.jochenhansoul.yummieapp.model.user.ConfirmationToken;
+import be.jochenhansoul.yummieapp.repository.ConfirmationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/*
+author: Kamer Elciyar
+ */
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository CONFIRMATION_TOKEN_REPOSITORY;
+
+    void saveConfirmationToken(ConfirmationToken confirmationToken) {
+        this.CONFIRMATION_TOKEN_REPOSITORY.save(confirmationToken);
+    }
+}
