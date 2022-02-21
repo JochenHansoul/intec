9489dc391b811ac2014e82dc912807330480cf36
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 18:40:05 +0100

    Added some commented code and made private final fields uppercase.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/config/WebSecurityConfig.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,12 +17,13 @@ author: Kamer Elciyar
 @AllArgsConstructor
 //@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserService USER_SERVICE;
+    private final BCryptPasswordEncoder BCRYPT_PASSWORD_ENCODER;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http/*.csrf().disable()*/
+                .authorizeRequests()
                 .antMatchers("/sign-up/**", "/sign-in/**")
                 .permitAll()
                 .anyRequest()
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(this.USER_SERVICE)
+                .passwordEncoder(this.BCRYPT_PASSWORD_ENCODER);
     }
 }
