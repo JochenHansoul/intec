d1fc6bf7de1ae28f0b22f9e9df26d6a2b80b07bb
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 16:25:20 +0100

    Made method public.

--- yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/config/WebSecurityConfig.java
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,12 +16,13 @@ author: Kamer Elciyar
 
 @Configuration
 @AllArgsConstructor
+//@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/sign-up/**", "/sign-in/**")
                 .permitAll()
