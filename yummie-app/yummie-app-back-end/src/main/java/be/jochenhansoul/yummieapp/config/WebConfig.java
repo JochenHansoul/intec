7f91336f76ffaeccaf3642303465c09e9c9fe501
JochenHansoul <jochen.hansoul@outlook.be>
2022-02-21 03:21:57 +0100

    Created WebConfig and WebSecurityConfig class.

---
+++ yummie-app/yummie-app-back-end/src/main/java/be/jochenhansoul/yummieapp/config/WebConfig.java
@@ -0,0 +1,19 @@
+package be.jochenhansoul.yummieapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*
+author: Kamer Elciyar
+ */
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+}
