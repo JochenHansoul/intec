c65a03624ddc72fc0f59ca2271fb07b2c5f0a8eb
JochenHansoul <jochen.hansoul@outlook.be>
2022-03-09 22:55:46 +0100

    Changed the classes of the coordinates to names and added class coordinates to connect them. In js used the names of the elemets to fetch them. Also changed restaurantName to name.

--- yummie-app/front-end/js/create-restaurant.js
+++ yummie-app/front-end/js/create-restaurant.js
@@ -3,13 +3,13 @@
 // constants
 const RESTAURANT_FORM = document.forms.restaurant;
 const INPUTS = RESTAURANT_FORM.getElementsByTagName("input");
-const OUTPUTS = RESTAURANT_FORM.querySelectorAll("output");
-const LATTITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(2) label input");
-const LONGITUDES = RESTAURANT_FORM.querySelectorAll("p:nth-child(3) label input");
+const LATTITUDES = RESTAURANT_FORM.querySelectorAll("p[name=\"lattitude\"] label input");
+const LONGITUDES = RESTAURANT_FORM.querySelectorAll("p[name=\"longitude\"] label input");
+const RESPONSE_OUTPUT = RESTAURANT_FORM.getElementsByTagName("output").response;
 
 // functions
-const sendResponse = x => OUTPUTS[2].innerText = "Restaurant succesfully saved";
-const sendError = x => OUTPUTS[2].innerText = "Could not save restaurant. Error: " + x.message;
+const sendResponse = x => RESPONSE_OUTPUT.innerText = "Restaurant succesfully saved";
+const sendError = x => RESPONSE_OUTPUT.innerText = "Could not save restaurant. Error: " + x.message;
 
 // main
 RESTAURANT_FORM.addEventListener("submit", event => {
@@ -17,7 +17,7 @@ RESTAURANT_FORM.addEventListener("submit", event => {
   // creating location
   const RESTAURANT = {
     idUser: 1,
-    name: INPUTS.restaurantName.value,
+    name: INPUTS.name.value,
     location: {
       lattitude: calculateLattitude(LATTITUDES[0].value, LATTITUDES[1].value, LATTITUDES[2].value),
       longitude: calculateLongitude(LONGITUDES[0].value, LONGITUDES[1].value, LONGITUDES[2].value)
